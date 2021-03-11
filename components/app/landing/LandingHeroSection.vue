d0162d511f7bfb86ca6e68a8bc65aeedadf068dc
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-03-11 21:37:55 +0100

    WIP feat (LandingHeroSection): add grid

--- components/app/landing/LandingHeroSection.vue
+++ components/app/landing/LandingHeroSection.vue
@@ -1,22 +1,40 @@
 <template>
-    <LandingSectionContainer>
-        <span>Test</span>
+  <LandingSectionContainer>
+    <div class="w-full h-64 grid grid-cols-2 gap-4">
+      <div>
+        <h1>
+          We are on track with the newest <span class="text-primary">projects</span> powered by the ocean
+          protocol.
+        </h1>
+        <p>
+          An absolute random example of some text being randomly put together to
+          make it look like someone has written something meaningful already.
+        </p>
         <AppButton />
-    </LandingSectionContainer>
+      </div>
+      <div>
+        <img
+          class="w-full"
+          src="../../../assets/images/kraken.webp"
+          alt="kraken"
+        />
+      </div>
+    </div>
+  </LandingSectionContainer>
 </template>
 
 <script>
-import AppButton from "@/components/common/AppButton.vue";
-import LandingSectionContainer from "./LandingSectionContainer";
+import AppButton from '@/components/common/AppButton.vue'
+import LandingSectionContainer from './LandingSectionContainer'
 
 export default {
-  name: "LandingHeroSection",
+  name: 'LandingHeroSection',
 
   components: {
     AppButton,
-    LandingSectionContainer
+    LandingSectionContainer,
   },
-};
+}
 </script>
 
 <style scoped></style>
