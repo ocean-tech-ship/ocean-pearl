5ffb2a6c6c2fb3dc2a2abecc2d0dfce084cedaa4
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-03-11 21:36:50 +0100

    feat (global): add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const colors = require('./tailwind.colors.json')
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      primary: colors.primary,
+      secondary: colors.secondary,
+      third: colors.third,
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
