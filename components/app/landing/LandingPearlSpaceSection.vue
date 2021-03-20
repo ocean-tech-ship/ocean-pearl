f84e67244578c4b152548c98a81aad4a8c47f241
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-03-20 18:36:38 +0100

    feat(LandingPearlSpaceSection): add pearl space section

---
+++ components/app/landing/LandingPearlSpaceSection.vue
@@ -0,0 +1,46 @@
+<template>
+  <LandingSectionContainer>
+    <h1>Pearl <span class="text-primary">space</span></h1>
+    <p>These are the main features of ocean pearl.</p>
+    <div class="flex flex-wrap">
+      <div v-for="card in cards" :key="card.title" class="card shadow">
+        <img>
+        {{ card.title }}
+      </div>
+    </div>
+  </LandingSectionContainer>
+</template>
+
+<script>
+import AppButton from '@/components/common/AppButton.vue'
+import LandingSectionContainer from './LandingSectionContainer'
+
+export default {
+  name: 'LandingPearlSpaceSection',
+
+  components: {
+    AppButton,
+    LandingSectionContainer,
+  },
+  data() {
+    return {
+      cards: [
+        {
+          title: 'Track Projects',
+          buttonTarget: '',
+          imageURL: '../../../assets/images/illustrations/track-projects.png',
+        },
+        { title: 'Submit Project', buttonTarget: '', imageURL: '' },
+        { title: 'Find Jobs', buttonTarget: '', imageURL: '' },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang='scss' scoped>
+.card {
+  height: 525px;
+  width: 362px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,22 @@
 <template>
   <div>
     <landing-hero-section />
+    <landing-pearl-space-section />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import LandingHeroSection from '@/components/app/landing/LandingHeroSection.vue'
+import LandingPearlSpaceSection from '@/components/app/landing/LandingPearlSpaceSection.vue'
 
 export default Vue.extend({
   components: {
     LandingHeroSection,
+    LandingPearlSpaceSection
   },
 })
 </script>
 
 <style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
 </style>
