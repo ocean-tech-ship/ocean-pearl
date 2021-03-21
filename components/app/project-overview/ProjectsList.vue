317d8a246934d1eeafa69d8bff4e68dc14a2af4d
moritzmurauer <moritzskaten@googlemail.com>
2021-03-21 19:53:08 +0100

    refactor(ProjectList.vue): new component for project-list

---
+++ components/app/project-overview/ProjectsList.vue
@@ -0,0 +1,298 @@
+<template>
+      <!-- projects ------------------------------------------------------------------>
+    <div class="grid grid-cols-5 gap-4 mt-12">
+
+      <!-- #1 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center relative">
+          <div class="absolute top-0 right-0 mr-3 flex space-x-2">
+            <img src="@/assets/images/icons/hiring-badge.png" alt="hiring job">
+            <img src="@/assets/images/icons/bitcoin-badge.png" alt="hiring job">
+          </div>
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+      <!-- #2 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #3 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+
+      <!-- #4 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #5 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #6 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+      <!-- #7 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #8 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+      <!-- #9 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #10 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+      <!-- #11 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+      <!-- #12 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #13 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+
+
+      <!-- #14 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+
+      <!-- #15 ------------------------------------------------------------------>
+      <div class="shadow p-2 pb-12 text-center">
+        <div class="mt-3">
+          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/jellyfish.png" alt="poseidon-network" />
+        </div>
+        <div class="mt-4">
+          <p class="text-primary p-line-head">jellyfish</p>
+          <p class="small-text">DeFi</p>
+        </div>
+        <div class="mt-6 px-3 flex place-content-center">
+          <p class="small-text">
+              This text describes the project
+              in one pretty cool sentence.
+          </p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
