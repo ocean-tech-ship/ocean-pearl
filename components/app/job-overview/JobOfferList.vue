666b5f2f0cbfedacdd95b3c3b486f9c2ecaedbde
moritzmurauer <moritzskaten@googlemail.com>
2021-03-21 20:41:42 +0100

    feat(JobOverview): add job-overview-page

---
+++ components/app/job-overview/JobOfferList.vue
@@ -0,0 +1,320 @@
+<template>
+  <!-- projects ------------------------------------------------------------------>
+  <div class="grid grid-cols-4 gap-4 mt-12">
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+    <!-------------------------------------------------------------------->
+    <div class="shadow p-12">
+      <div class="flex">
+        <div class="mr-3">
+          <img class="inline-block h-10 w-10 rounded-full ring-2 ring-white"
+            src="@/assets/images/project-logos/poseidon.png" alt="poseidon-network" />
+        </div>
+        <div>
+          <p class="text-primary p-line-head">poseidon</p>
+          <p class="small-text ">Data analyst</p>
+        </div>
+      </div>
+      <div class="mt-5">
+        <p class="small-text">
+          Data analyst who is on track with registring new
+          data assets and maintaining them aswell.
+        </p>
+      </div>
+      <div class="mt-8">
+        <p class="small-text text-primary">
+          50000€ - 60000€ a year
+        </p>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style>
+  .p-line-head {
+    line-height: 20px !important;
+  }
+
+</style>
