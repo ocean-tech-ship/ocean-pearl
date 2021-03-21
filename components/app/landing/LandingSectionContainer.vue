7e1a0e56170a0b0fabb187cca7e696eded6a6e45
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-03-21 19:16:02 +0100

    feat(Landing): improve responsive behavior

--- components/app/landing/LandingSectionContainer.vue
+++ components/app/landing/LandingSectionContainer.vue
@@ -5,7 +5,7 @@ export default {
       'section',
       {
         class: {
-          'px-20 sm:px-30 mx-auto max-w-1440': true,
+          'px-1 mb-12 2sm:px-4 sm:px-16 md:px-32 mx-auto max-w-1440': true,
         },
       },
       this.$slots.default
