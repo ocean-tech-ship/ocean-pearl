277b406af043d30b1cc03381986b462c9b35dbb1
moritzmurauer <moritzskaten@googlemail.com>
2021-03-24 23:04:33 +0100

    refactor(JobOfferList): update path to detail-page

--- components/app/job-overview/JobOfferList.vue
+++ components/app/job-overview/JobOfferList.vue
@@ -3,7 +3,7 @@
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 mt-12">
     <div v-for="project in projects" :key="project.title">
       <!-- single ------------------------------------------------------------------>
-      <NuxtLink to="/job-overview">
+      <NuxtLink to="/project-detail">
         <div class="shadow p-12">
           <div class="flex">
             <div class="mr-3">
