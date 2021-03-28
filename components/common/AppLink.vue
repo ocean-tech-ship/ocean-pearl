ff44aefdd76b608621c4e55f1939d171fb8e4396
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-03-28 13:29:01 +0200

    fix(AppLink): layout fix

--- components/common/AppLink.vue
+++ components/common/AppLink.vue
@@ -1,8 +1,15 @@
 <template>
-  <nuxt-link v-if="isNuxtLink" :to="to"><slot></slot></nuxt-link>
-  <a v-else :href="to" target="_blank" rel="noopener noreferrer"
+  <nuxt-link v-if="isNuxtLink" class="inline-block" :to="to"
     ><slot></slot
-  ></a>
+  ></nuxt-link>
+  <a
+    v-else
+    class="inline-block"
+    :href="to"
+    target="_blank"
+    rel="noopener noreferrer"
+    ><slot></slot>
+  </a>
 </template>
 
 <script>
@@ -14,6 +21,7 @@ export default {
       if (this.to) {
         return this.to.substring(0, 1) === '/'
       }
+      return false
     },
   },
 }
