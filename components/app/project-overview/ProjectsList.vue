f0baedf627f00007409892d27f0d46f62e9eb632
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-04-18 16:49:59 +0200

    fix(ProjectsList): add import SectionContainer

--- components/app/project-overview/ProjectsList.vue
+++ components/app/project-overview/ProjectsList.vue
@@ -1,50 +1,57 @@
 <template>
   <LandingSectionContainer>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mt-12">
-    <div v-for="project in projects" :key="project.id">
-      <div class="shadow p-2 pb-12 text-center relative">
-        <div class="absolute top-0 right-0 mr-3 flex space-x-2">
-          <img
-            v-if="project.badgeJob"
-            src="@/assets/images/icons/hiring-badge.png"
-            alt="hiring job"
-          />
-          <img
-            v-if="project.badgeFund"
-            src="@/assets/images/icons/bitcoin-badge.png"
-            alt="looking for funding"
-          />
-          <img
-            v-if="project.badgeFeatured"
-            src="@/assets/images/icons/featured-badge.png"
-            alt="featured project"
-          />
-        </div>
-        <div class="mt-3">
-          <img
-            class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
-            :src="project.logo"
-            :alt="project.title"
-          />
-        </div>
-        <div class="mt-4">
-          <p class="text-primary p-line-head">{{ project.title }}</p>
-          <p class="small-text">{{ project.category }}</p>
-        </div>
-        <div class="mt-6 px-3 flex place-content-center">
-          <p class="small-text">
-            {{ project.description }}
-          </p>
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mt-12"
+    >
+      <div v-for="project in projects" :key="project.id">
+        <div class="shadow p-2 pb-12 text-center relative">
+          <div class="absolute top-0 right-0 mr-3 flex space-x-2">
+            <img
+              v-if="project.badgeJob"
+              src="@/assets/images/icons/hiring-badge.png"
+              alt="hiring job"
+            />
+            <img
+              v-if="project.badgeFund"
+              src="@/assets/images/icons/bitcoin-badge.png"
+              alt="looking for funding"
+            />
+            <img
+              v-if="project.badgeFeatured"
+              src="@/assets/images/icons/featured-badge.png"
+              alt="featured project"
+            />
+          </div>
+          <div class="mt-3">
+            <img
+              class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+              :src="project.logo"
+              :alt="project.title"
+            />
+          </div>
+          <div class="mt-4">
+            <p class="text-primary p-line-head">{{ project.title }}</p>
+            <p class="small-text">{{ project.category }}</p>
+          </div>
+          <div class="mt-6 px-3 flex place-content-center">
+            <p class="small-text">
+              {{ project.description }}
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   </LandingSectionContainer>
 </template>
 
 <script>
+import LandingSectionContainer from '../landing/LandingSectionContainer'
 export default {
   name: 'ProjectsList',
+  components: {
+    LandingSectionContainer,
+  },
+
   data() {
     return {
       projects: [],
