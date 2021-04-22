7b25f2876cd951028f602e28b701f8f2f02fcd41
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-04-22 21:34:41 +0200

    feat(ProjectsList) add AppLink to dynamc project page

--- components/app/project-overview/ProjectsList.vue
+++ components/app/project-overview/ProjectsList.vue
@@ -1,27 +1,46 @@
 <template>
   <LandingSectionContainer>
-  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mt-12">
-    <div v-for="project in projects" :key="project.title">
-      <div class="shadow p-2 pb-12 text-center relative">
-        <div class="absolute top-0 right-0 mr-3 flex space-x-2">
-          <img v-if="project.badgeJob" src="@/assets/images/icons/hiring-badge.svg" alt="hiring job">
-          <img v-if="project.badgeFund" src="@/assets/images/icons/bitcoin-badge.svg" alt="looking for funding">
-          <img v-if="project.badgeFeatured" src="@/assets/images/icons/featured-badge.svg" alt="featured project">
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 mt-12"
+    >
+      <div v-for="project in projects" :key="project.title">
+        <AppLink :to="`/project-overview/${project.id}`">
+        <div class="shadow p-2 pb-12 text-center relative">
+          <div class="absolute top-0 right-0 mr-3 flex space-x-2">
+            <img
+              v-if="project.badgeJob"
+              src="@/assets/images/icons/hiring-badge.svg"
+              alt="hiring job"
+            />
+            <img
+              v-if="project.badgeFund"
+              src="@/assets/images/icons/bitcoin-badge.svg"
+              alt="looking for funding"
+            />
+            <img
+              v-if="project.badgeFeatured"
+              src="@/assets/images/icons/featured-badge.svg"
+              alt="featured project"
+            />
+          </div>
+          <div class="mt-3">
+            <img
+              class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+              :src="project.logo"
+              :alt="project.title"
+            />
+          </div>
+          <div class="mt-4">
+            <p class="text-primary p-line-head">{{ project.title }}</p>
+            <p class="small-text">{{ project.category }}</p>
+          </div>
+          <div class="mt-6 px-3 flex place-content-center">
+            <p class="small-text">
+              {{ project.description }}
+            </p>
+          </div>
         </div>
-        <div class="mt-3">
-          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white" :src="project.imageURL"
-            :alt="project.title" />
-        </div>
-        <div class="mt-4">
-          <p class="text-primary p-line-head">{{project.title}}</p>
-          <p class="small-text">{{project.category}}</p>
-        </div>
-        <div class="mt-6 px-3 flex place-content-center">
-          <p class="small-text">
-            {{project.description}}
-          </p>
-        </div>
-        </NuxtLink>
+        </AppLink>
       </div>
     </div>
   </LandingSectionContainer>
@@ -29,12 +48,14 @@
 
 <script>
 import LandingSectionContainer from '../landing/LandingSectionContainer'
+import AppLink from '@/components/common/AppLink'
 import { getProjects } from '@/api.js'
 
 export default {
   name: 'ProjectsList',
   components: {
     LandingSectionContainer,
+    AppLink
   },
   data() {
     return {
