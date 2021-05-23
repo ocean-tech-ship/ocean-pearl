7ae04a0b5d8e2498b1f83b3e47fbf61c46b84724
Marcel Haßlinger <marcel.hasslinger@gmx.de>
2021-05-23 12:45:18 +0200

    feat(ProjectSingleDetailsMember): show project team member with optional third party social links

---
+++ components/app/project-detail/ProjectSingleDetailsMember.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex items-center flex-wrap">
+    <div class="flex items-center">
+      <img
+        class="h-16px w-16px mr-2"
+        src="@/assets/images/detail/member.svg"
+        :alt="$t('project.team') + ' ' + $t('general.icon')"
+      />
+
+      <span class="small-text">
+        {{ member.name + ', ' + member.purpose }}
+      </span>
+    </div>
+
+    <div class="flex">
+      <span
+        v-for="social in member.socials"
+        :key="social.type"
+        class="small-text"
+      >
+        &nbsp;(<a
+          class="text-primary small-text"
+          :href="social.url"
+          target="_blank"
+          >{{ social.type }}</a
+        >)
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectSingleDetailsMember',
+
+  props: {
+    member: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
