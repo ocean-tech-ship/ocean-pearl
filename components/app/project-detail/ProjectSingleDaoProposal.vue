5bb61f4f1f7b77404770ec4baffeea755c32ef07
Marcel Haßlinger <marcel.hasslinger@gmx.de>
2021-05-23 12:47:45 +0200

    feat(ProjectSingleDaoProposal): apply changes for dao mvp

--- components/app/project-detail/ProjectSingleDaoProposal.vue
+++ components/app/project-detail/ProjectSingleDaoProposal.vue
@@ -1,75 +1,136 @@
 <template>
-  <div>
+  <landing-section-container>
     <h4>DAO <span class="text-primary">proposal</span></h4>
 
-    <div class="flex flex-col justify-between md:flex-row mt-8">
-      <!-- Start Date -->
-      <div class="flex justify-between my-1 md:flex-col">
-        <div class="flex">
-          <img
-            class="mr-1"
-            src="@/assets/images/icons/start-fund.svg"
-            alt="funding-start"
-          />
+    <div class="grid lg:grid-cols-2 gap-4 mt-6">
+      <div class="grid lg:grid-cols-2 gap-4">
+        <!-- Funding Round -->
+        <div class="shadow rounded p-2">
+          <div class="flex items-center">
+            <img
+              class="h-16px w-16px mr-2"
+              src="@/assets/images/detail/fund-round.svg"
+              :alt="$t('project.proposal.round') + ' ' + $t('general.icon')"
+            />
 
-          <p class="text-primary">Start</p>
+            <p class="small-text text-primary">
+              {{ $t('project.proposal.round') }}
+            </p>
+          </div>
+
+          <p class="small-text">{{ round }}</p>
         </div>
 
-        <p class="small-text" v-text="start" />
-      </div>
+        <!-- Requested Funding -->
+        <div class="shadow rounded p-2">
+          <div class="flex items-center">
+            <img
+              class="h-16px w-16px mr-2"
+              src="@/assets/images/detail/fund-requested.svg"
+              :alt="$t('project.proposal.requested') + ' ' + $t('general.icon')"
+            />
 
-      <!-- Finish Date -->
-      <div class="flex justify-between my-1 md:flex-col">
-        <div class="flex">
-          <img
-            class="mr-1"
-            src="@/assets/images/icons/finish-fund.svg"
-            alt="funding-finish"
-          />
+            <p class="small-text text-primary">
+              {{ $t('project.proposal.requested') }}
+            </p>
+          </div>
 
-          <p class="text-primary">Finish</p>
+          <p class="small-text">
+            {{ requested }}
+          </p>
         </div>
 
-        <p class="small-text" v-text="finish" />
-      </div>
+        <!-- Proposal Wallet Address -->
+        <div class="shadow rounded p-2 lg:col-span-2">
+          <p class="small-text text-primary">
+            {{ $t('project.proposal.wallet') }}
+          </p>
 
-      <!-- Fund Round -->
-      <div class="flex justify-between my-1 md:flex-col">
-        <div class="flex">
-          <img
-            class="mr-1"
-            src="@/assets/images/icons/fund-round.svg"
-            alt="funding-round"
-          />
-
-          <p class="text-primary">Fund round</p>
+          <p class="small-text break-all">{{ wallet }}</p>
         </div>
 
-        <p class="small-text" v-text="round" />
+        <!-- Action (lg only) -->
+        <div class="hidden lg:block lg:col-span-2">
+          <app-button-style
+            :icon="require('@/assets/images/detail/fund-here.svg')"
+            :text="$t('project.proposal.vote')"
+          />
+        </div>
       </div>
 
-      <!-- Fund Action -->
-      <div class="mt-5">
-        <AppButtonStyle
+      <div class="grid lg:flex lg:justify-end">
+        <!-- Total Fundings -->
+        <div>
+          <div
+            class="shadow rounded p-2 overflow-y-auto"
+            style="max-height: 13rem"
+          >
+            <div class="flex items-center">
+              <img
+                class="h-16px w-16px mr-2"
+                src="@/assets/images/detail/fund-finish.svg"
+                :alt="$t('project.proposal.total') + ' ' + $t('general.icon')"
+              />
+
+              <p class="small-text text-primary">
+                {{ $t('project.proposal.total') }}
+              </p>
+            </div>
+
+            <p
+              v-for="fundRound in rounds"
+              :key="fundRound.name"
+              class="small-text pr-8"
+            >
+              {{ fundRound.funding + ' (' + fundRound.name + ')' }}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="lg:hidden">
+        <app-button-style
           :icon="require('@/assets/images/detail/fund-here.svg')"
-          :text="$t('project_detail.fundHereButton')"
+          :text="$t('project.proposal.vote')"
         />
       </div>
     </div>
-  </div>
+  </landing-section-container>
 </template>
 
 <script>
 import AppButtonStyle from '@/components/common/AppButtonStyle'
+import LandingSectionContainer from '~/components/app/landing/LandingSectionContainer'
 
 export default {
   name: 'ProjectSingleDaoProposal',
 
   components: {
     AppButtonStyle,
+    LandingSectionContainer,
   },
 
-  props: ['start', 'finish', 'round'],
+  props: {
+    round: {
+      type: String,
+      required: true,
+    },
+
+    requested: {
+      type: String,
+      required: true,
+    },
+
+    wallet: {
+      type: String,
+      required: true,
+    },
+
+    rounds: {
+      type: Array,
+      required: true,
+    },
+  },
 }
 </script>
 
