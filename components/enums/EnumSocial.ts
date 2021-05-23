cdf8744a0f73a638af4e446ea28f528897f1d8c1
Marcel Haßlinger <marcel.hasslinger@gmx.de>
2021-05-23 12:43:00 +0200

    feat(EnumSocial): add enum which represents third party socials

---
+++ components/enums/EnumSocial.ts
@@ -0,0 +1,12 @@
+enum EnumSocial {
+  WEBSITE = 'Website',
+  GITHUB = 'GitHub',
+  TWITTER = 'Twitter',
+  LINKEDIN = 'LinkedIn',
+  REDDIT = 'reddit',
+  FACEBOOK = 'Facebook',
+  TELEGRAM = 'Telegram',
+  DISCORD = 'Discord',
+}
+
+export default EnumSocial
