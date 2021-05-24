3e51e29716973659f59e139409caf5c399d2fd50
moritzmurauer <moritzskaten@googlemail.com>
2021-05-24 18:19:55 +0200

    feat(DaoProjectList): create new route folder for dao projects

---
+++ pages/dao-project-overview/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <dao-projects-header />
+    <dao-projects-list />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import DaoProjectsHeader from '@/components/app/dao-project-overview/DaoProjectsHeader.vue'
+import DaoProjectsList from '@/components/app/dao-project-overview/DaoProjectsList.vue'
+
+export default Vue.extend({
+
+  name: "DaoProjectOverview",
+  components: {
+    DaoProjectsHeader,
+    DaoProjectsList
+  },
+})
+</script>
+
+<style>
+</style>
