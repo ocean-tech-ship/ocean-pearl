b6954ba7615ea67751150bf2e567f4fc6aaa1b21
Marcel Haßlinger <marcel.hasslinger@gmx.de>
2021-05-24 13:46:53 +0200

    refactor(SocialMedia): cleanup code and remove unnecessary

--- model/SocialMedia.ts
+++ model/SocialMedia.ts
@@ -1,6 +1,6 @@
 export class SocialMedia {
   public static parse(id: string): SocialMedia {
-    switch (id) {
+    switch (id.toLowerCase()) {
       case 'website':
         return this.WEBSITE
       case 'github':
@@ -22,56 +22,17 @@ export class SocialMedia {
     }
   }
 
-  public static WEBSITE = new SocialMedia(
-    'WEBSITE',
-    'Website',
-    require('@/assets/images/social/website.svg')
-  )
-
-  public static GITHUB = new SocialMedia(
-    'GITHUB',
-    'GitHub',
-    require('@/assets/images/social/github.svg')
-  )
-
-  public static TWITTER = new SocialMedia(
-    'TWITTER',
-    'Twitter',
-    require('@/assets/images/social/twitter.svg')
-  )
-
-  public static LINKEDIN = new SocialMedia(
-    'LINKEDIN',
-    'LinkedIn',
-    require('@/assets/images/social/linkedin.svg')
-  )
-
-  public static REDDIT = new SocialMedia(
-    'REDDIT',
-    'Reddit',
-    require('@/assets/images/social/reddit.svg')
-  )
-
-  public static FACEBOOK = new SocialMedia(
-    'FACEBOOK',
-    'Facebook',
-    require('@/assets/images/social/facebook.svg')
-  )
-
-  public static TELEGRAM = new SocialMedia(
-    'TELEGRAM',
-    'Telegram',
-    require('@/assets/images/social/telegram.svg')
-  )
-
-  public static DISCORD = new SocialMedia(
-    'DISCORD',
-    'Discord',
-    require('@/assets/images/social/discord.svg')
-  )
+  public static WEBSITE = new SocialMedia('Website')
+  public static GITHUB = new SocialMedia('GitHub')
+  public static TWITTER = new SocialMedia('Twitter')
+  public static LINKEDIN = new SocialMedia('LinkedIn')
+  public static REDDIT = new SocialMedia('Reddit')
+  public static FACEBOOK = new SocialMedia('Facebook')
+  public static TELEGRAM = new SocialMedia('Telegram')
+  public static DISCORD = new SocialMedia('Discord')
 
   public get Id(): string {
-    return this.id
+    return this.title.toUpperCase()
   }
 
   public get Title(): string {
@@ -79,13 +40,9 @@ export class SocialMedia {
   }
 
   public get Icon(): string {
-    return this.icon
+    return require(`@/assets/images/social/${this.title.toLowerCase()}.svg`)
   }
 
   // eslint-disable-next-line no-useless-constructor
-  constructor(
-    private id: string,
-    private title: string,
-    private icon: string
-  ) {}
+  constructor(private title: string) {}
 }
