6da8558c352d2d304c509bef013debf48314d753
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-05-24 21:17:14 +0200

    fix(truncate-filter): fix crash when text is missing

--- plugins/vue-globals.js
+++ plugins/vue-globals.js
@@ -3,6 +3,9 @@ import Vue from "vue";
 export default (ctx, inject) => {
   //Global truncate filter
   Vue.filter('truncate', function (text, stop, clamp) {
-    return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+    if(text && stop){
+      return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+    }
+    return ''
   })
 }
