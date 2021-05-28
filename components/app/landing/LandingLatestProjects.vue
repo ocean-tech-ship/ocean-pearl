b7b04be3827558635946875e94ab521942b8594c
Marcel Haßlinger <marcel.hasslinger@gmx.de>
2021-05-28 17:46:25 +0200

    feat(LandingLatestProjects): localize submit timestamp distance

--- components/app/landing/LandingLatestProjects.vue
+++ components/app/landing/LandingLatestProjects.vue
@@ -1,90 +1,115 @@
 <template>
   <LandingSectionContainer class="my-32">
-    <h2>{{ $t('landing.latest_projects.title') }} <span class="text-primary">{{ $t('landing.latest_projects.titleHighlight') }}</span></h2>
+    <h2>
+      {{ $t('landing.latest_projects.title') }}
+      <span class="text-primary">{{
+        $t('landing.latest_projects.titleHighlight')
+      }}</span>
+    </h2>
     <p>{{ $t('landing.latest_projects.text') }}</p>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 mt-10">
+    <div
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 mt-10"
+    >
       <div v-for="project in projects" :key="project.title">
-      <NuxtLink :prefetch="false" to="/project-overview">
-      <div class="shadow rounded p-4 pb-12 h-275px text-center">
-        <div class="mt-3">
-          <img class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
-            :src="project.imageURL" :alt="project.title" />
-        </div>
-        <div class="mt-4">
-          <p class="text-primary p-line-head">{{project.title | truncate(14)}}</p>
-          <p class="small-text">{{project.category}}</p>
-        </div>
-        <div class="mt-4 flex place-content-center">
-          <p class="border small-text text-primary w-32">{{project.createdAt}}</p>
-        </div>
+        <NuxtLink :prefetch="false" to="/project-overview">
+          <div class="shadow rounded p-4 pb-12 h-275px text-center">
+            <div class="mt-3">
+              <img
+                class="inline-block h-16 w-16 rounded-full ring-2 ring-white"
+                :src="project.imageURL"
+                :alt="project.title"
+              />
+            </div>
+            <div class="mt-4">
+              <p class="text-primary p-line-head">
+                {{ project.title | truncate(14) }}
+              </p>
+              <p class="small-text">{{ project.category }}</p>
+            </div>
+            <div class="mt-4 flex place-content-center">
+              <p class="border small-text text-primary w-32">
+                {{ formatDistance(project.createdAt) }}
+              </p>
+            </div>
+          </div>
+        </NuxtLink>
       </div>
-      </NuxtLink>
-     </div>
     </div>
     <NuxtLink :prefetch="false" to="/project-overview">
       <div class="flex items-center mt-6 mb-32">
-        <p class="mr-2 text-primary">{{ $t('landing.latest_projects.link_text') }}</p>
-        <img src="@/assets/images/landing/check-out.svg" alt="arrow">
+        <p class="mr-2 text-primary">
+          {{ $t('landing.latest_projects.link_text') }}
+        </p>
+        <img src="@/assets/images/landing/check-out.svg" alt="arrow" />
       </div>
     </NuxtLink>
   </LandingSectionContainer>
 </template>
 
 <script>
-  import LandingSectionContainer from './LandingSectionContainer'
+import LandingSectionContainer from './LandingSectionContainer'
 
-  export default {
-    name: 'LandingDaoProposal',
+export default {
+  name: 'LandingDaoProposal',
 
-    components: {
-      LandingSectionContainer,
+  components: {
+    LandingSectionContainer,
+  },
+  data() {
+    return {
+      projects: [
+        {
+          title: 'GreenSquareExample',
+          category: 'LoFi',
+          createdAt: '2021-05-08T21:39:46.378Z',
+          projectTarget: '',
+          imageURL: require('@/assets/images/poseidon-network.png'),
+        },
+        {
+          title: 'Poseidon',
+          category: 'LoFi',
+          createdAt: Date.now(),
+          projectTarget: '',
+          imageURL: require('@/assets/images/poseidon.png'),
+        },
+        {
+          title: 'Jellyfish',
+          category: 'LoFi',
+          createdAt: '2021-05-27T21:39:46.378Z',
+          projectTarget: '',
+          imageURL: require('@/assets/images/jellyfish.png'),
+        },
+        {
+          title: 'Pearl',
+          category: 'LoFi',
+          createdAt: '2021-04-08T21:39:46.378Z',
+          projectTarget: '',
+          imageURL: require('@/assets/images/logo/pearl-logo.svg'),
+        },
+        {
+          title: 'Poseidon',
+          category: 'LoFi',
+          createdAt: '2021-05-20T21:39:46.378Z',
+          projectTarget: '',
+          imageURL: require('@/assets/images/poseidon.png'),
+        },
+      ],
+    }
+  },
+
+  methods: {
+    formatDistance(timestamp) {
+      return this.$dateFns.formatDistanceToNowStrict(new Date(timestamp), {
+        addSuffix: true,
+        locale: this.$i18n.locale,
+      })
     },
-    data() {
-      return {
-        projects: [
-          {
-            title: 'GreenSquareExample',
-            category: 'LoFi',
-            createdAt: '2h ago',
-            projectTarget: '',
-            imageURL: require('@/assets/images/poseidon-network.png'),
-          },
-          {
-            title: 'Poseidon',
-            category: 'LoFi',
-            createdAt: '2h ago',
-            projectTarget: '',
-            imageURL: require('@/assets/images/poseidon.png'),
-          },
-          {
-            title: 'Jellyfish',
-            category: 'LoFi',
-            createdAt: '8h ago',
-            projectTarget: '',
-            imageURL: require('@/assets/images/jellyfish.png'),
-          },
-          {
-            title: 'Pearl',
-            category: 'LoFi',
-            createdAt: '1 day ago',
-            projectTarget: '',
-            imageURL: require('@/assets/images/logo/pearl-logo.svg'),
-          },
-          {
-            title: 'Poseidon',
-            category: 'LoFi',
-            createdAt: '9h ago',
-            projectTarget: '',
-            imageURL: require('@/assets/images/poseidon.png'),
-          },
-        ],
-      }
-    },
-  }
+  },
+}
 </script>
 
 <style scoped>
-  .p-line-head {
-    line-height: 20px;
-  }
+.p-line-head {
+  line-height: 20px;
+}
 </style>
