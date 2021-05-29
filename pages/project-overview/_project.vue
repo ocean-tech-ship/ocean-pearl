4020f8a65dc4ac9148979da21bd82750cf9fa16e
Marcel Haßlinger <marcel.hasslinger@gmx.de>
2021-05-29 11:12:45 +0200

    feat(_project): add error handling

--- pages/project-overview/_project.vue
+++ pages/project-overview/_project.vue
@@ -1,28 +1,47 @@
 <template>
   <div>
-    <project-single-header :project="project" />
-    <project-single :project="project" />
+    <div v-if="!!error">
+      <landing-section-container>
+        <h1 class="text-primary">{{ $t('general.fetchingError') }}</h1>
+        <p class="small-text">{{ $t(error) }}</p>
+      </landing-section-container>
+    </div>
+
+    <div v-if="!!project">
+      <project-single-header :project="project" />
+      <project-single :project="project" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-
+import { getProjectById } from '@/api.js'
+import LandingSectionContainer from '~/components/app/landing/LandingSectionContainer.vue'
 import ProjectSingleHeader from '~/components/app/project-detail/ProjectSingleHeader.vue'
 import ProjectSingle from '~/components/app/project-detail/ProjectSingle.vue'
-import { getProjectById } from '@/api.js'
 
 export default Vue.extend({
   name: 'ProjectDetail',
   components: {
     ProjectSingle,
     ProjectSingleHeader,
+    LandingSectionContainer,
   },
 
   async asyncData({ $axios, params }) {
-    const project = await getProjectById($axios, params.project)
-    return process.env.useMirage === 'true' ? project :  { project }
-  }
+    try {
+      const response = await getProjectById($axios, params.project)
+
+      if (response.status === 204) {
+        return { error: 'general.error.unknown', project: null }
+      }
+
+      return { error: null, project: response.data }
+    } catch (error) {
+      return { error: 'general.error.retry', project: null }
+    }
+  },
 })
 </script>
 
