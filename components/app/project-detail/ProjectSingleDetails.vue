be3be4c79edf3f6a0a4bc7b393fc3d47bdc52576
Marcel Haßlinger <mysteryplays@gmx.de>
2021-05-30 21:08:55 +0200

    feat(ProjectSingleDetails): hide team completely if not present

--- components/app/project-detail/ProjectSingleDetails.vue
+++ components/app/project-detail/ProjectSingleDetails.vue
@@ -9,10 +9,8 @@
         <p class="small-text">{{ project.description }}</p>
 
         <!-- Team -->
-        <div class="mb-10">
+        <div v-if="!!project.team" class="mb-10">
           <p class="text-primary small-text mt-6">{{ $t('project.team') }}</p>
-
-          <p v-if="!project.team" class="small-text">/</p>
 
           <project-single-details-member
             v-for="(member, index) in project.team"
