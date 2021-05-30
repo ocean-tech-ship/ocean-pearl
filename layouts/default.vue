47ae27d61ac1415feb78a897cf4574d0a6f7928e
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-05-30 22:21:03 +0200

    refactor: deactivate footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,18 @@
   <div>
     <app-navbar />
     <Nuxt />
-    <footer-container />
+    <!-- <footer-container /> -->
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import AppNavbar from '@/components/layout/AppNavbar.vue'
-import FooterContainer from '@/components/app/footer/FooterContainer.vue'
+// import FooterContainer from '@/components/app/footer/FooterContainer.vue'
 export default Vue.extend({
   components: {
     AppNavbar,
-    FooterContainer
+    // FooterContainer
   },
 })
 </script>
