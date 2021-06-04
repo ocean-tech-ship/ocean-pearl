21211077f485945b30d32c311fbea05bb0393b61
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-06-04 21:51:58 +0200

    fix(LandingPearlDao): add border radius

--- components/app/landing/LandingPearlDao.vue
+++ components/app/landing/LandingPearlDao.vue
@@ -12,7 +12,7 @@
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 mt-10"
     >
       <div v-for="card in daoInfoCards" :key="card.title">
-        <div class="shadow p-4 pb-4">
+        <div class="rounded shadow p-4 pb-4">
           <div class="flex align-center">
             <div class="mr-3">
               <img
