399d6277bd6312597179bdf2b7bf1aaa0188a894
mr-pandy <ekerekepreciousimeh@gmail.com>
2021-06-07 20:26:17 +0100

    fix(footer): change footer structure for dynmaic content

--- components/app/footer/FooterBlock.vue
+++ components/app/footer/FooterBlock.vue
@@ -1,50 +1,35 @@
 <template>
   <div class="lg:flex lg:gap-10">
-    <a class="table mx-auto" :href="items[0].route" >
-      <div class="lg:flex lg:items-center lg:justify-center lg:gap-2">
+    <AppLink class="table mx-auto" :to="item.route">
+      <div class="lg:flex lg:items-center lg:justify-center">
         <img
-          :src="items[0].icon"
-          :alt="items[0].name + ' logo'"
-          class="lg:w-6 lg:h-6 w-10 h-10 block mx-auto"
+          :src="item.icon"
+          :alt="item.name + ' logo'"
+          class="lg:w-6 lg:h-6 w-10 h-10 block lg:mr-2 mx-auto"
         />
         <p :class="`mt-0 lg:mt-0 font-semibold ${theme} lg:text-sm`">
-          {{ items[0].name }}
+          {{ item.name }}
         </p>
       </div>
-    </a>
+    </AppLink>
 
-    <div class="table mx-auto md:mt-0 mt-4">
-      <div class="grid sm:grid-cols-2 gap-2 sm:gap-10">
-        <a
-          :href="items[1].route"
-          rel="noopener noreferrer"
-          target="_blank"
+    <div v-if="item.hasSubItem" class="table mx-auto md:mt-0 mt-4">
+      <div :class="`grid sm:grid-cols-${item.subItems.length} gap-2 sm:gap-10`">
+        <AppLink
+          v-for="subItem in item.subItems"
+          :key="subItem.id"
+          :to="subItem.route"
           class="table mx-auto"
         >
-          <div class="flex items-center justify-center gap-2">
+          <div class="flex items-center justify-center">
             <img
-              :src="items[1].icon"
-              :alt="items[1].name + ' logo'"
-              class="w-6 h-6"
+              :src="subItem.icon"
+              :alt="subItem.name + ' logo'"
+              class="w-6 h-6 mr-2"
             />
-            <p class="text-sm">{{ items[1].name }}</p>
+            <p class="text-sm">{{ subItem.name }}</p>
           </div>
-        </a>
-        <a
-          :href="items[2].route"
-          rel="noopener noreferrer"
-          target="_blank"
-          class="table mx-auto"
-        >
-          <div class="flex items-center justify-center gap-2">
-            <img
-              :src="items[2].icon"
-              :alt="items[2].name + ' logo'"
-              class="w-6 h-6"
-            />
-            <p class="text-sm">{{ items[2].name }}</p>
-          </div>
-        </a>
+        </AppLink>
       </div>
     </div>
   </div>
@@ -52,11 +37,13 @@
 
 <script>
 import Vue from 'vue'
+import AppLink from '~/components/common/AppLink.vue'
 
 export default Vue.extend({
+  components: { AppLink },
   props: {
-    items: {
-      type: Array,
+    item: {
+      type: Object,
       required: true,
     },
     theme: {
