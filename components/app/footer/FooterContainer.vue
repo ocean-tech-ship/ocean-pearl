048c12703dfb67ae4617ce46ab727891276df0e8
mr-pandy <ekerekepreciousimeh@gmail.com>
2021-06-07 20:26:17 +0100

    fix(footer): change footer structure for dynmaic content

--- components/app/footer/FooterContainer.vue
+++ components/app/footer/FooterContainer.vue
@@ -1,15 +1,17 @@
 <template>
-  <footer class="w-full bg-secondary">
+  <footer class="w-full">
     <div
       class="w-full max-w-1440 mx-auto py-10 grid gap-10 md:flex md:items-center md:gap-20 md:justify-center lg:gap-5"
     >
-      <footer-block theme="text-primary" :items="footerItems.slice(0, 3)" />
-      <img
-        :src="footerDivider.icon"
-        :alt="`${footerDivider.name} logo`"
-        class="w-10 h-10 lg:block hidden"
-      />
-      <footer-block :items="footerItems.slice(4, 7)" />
+      <div v-for="item in footerItems" :key="item.id">
+        <img
+          v-if="item.id === 'supreme'"
+          :src="item.icon"
+          :alt="`${item.name} logo`"
+          class="w-10 h-10 lg:block hidden"
+        />
+        <footer-block v-else theme="text-primary" :item="item" />
+      </div>
     </div>
   </footer>
 </template>
@@ -28,20 +30,23 @@ export default Vue.extend({
         icon: require('@/assets/images/footer/ocean_pearl_footer_logo.svg'),
         route: '/',
         block: true,
-      },
-      {
-        id: 1,
-        name: 'Twitter',
-        icon: require('@/assets/images/footer/twitter_footer.svg'),
-        route: 'https://twitter.com/oceanpearlio',
-        ext: true,
-      },
-      {
-        id: 2,
-        name: 'Discord',
-        icon: require('@/assets/images/footer/discord_footer.svg'),
-        route: 'https://discord.gg/eswPj8QZRH',
-        ext: true,
+        hasSubItem: true,
+        subItems: [
+          {
+            id: 0,
+            name: 'Twitter',
+            icon: require('@/assets/images/footer/twitter_footer.svg'),
+            route: 'https://twitter.com/oceanpearlio',
+            ext: true,
+          },
+          {
+            id: 1,
+            name: 'Discord',
+            icon: require('@/assets/images/footer/discord_footer.svg'),
+            route: 'https://discord.gg/eswPj8QZRH',
+            ext: true,
+          },
+        ],
       },
       {
         id: 'supreme',
@@ -50,35 +55,32 @@ export default Vue.extend({
         route: '/',
       },
       {
-        id: 3,
+        id: 1,
         name: 'Ocean Protocol',
         icon: require('@/assets/images/footer/protocol_footer.svg'),
         route: 'https://oceanprotocol.com/',
         ext: true,
         block: true,
-      },
-      {
-        id: 4,
-        name: 'Whitepaper',
-        icon: require('@/assets/images/footer/white_paper_footer.svg'),
-        route: 'https://oceanprotocol.com/tech-whitepaper.pdf',
-        ext: true,
-      },
-      {
-        id: 5,
-        name: 'Discord',
-        icon: require('@/assets/images/footer/discord_footer.svg'),
-        route: 'https://discord.gg/Yuawx9EP6e',
-        ext: true,
+        hasSubItem: true,
+        subItems: [
+          {
+            id: 4,
+            name: 'Whitepaper',
+            icon: require('@/assets/images/footer/white_paper_footer.svg'),
+            route: 'https://oceanprotocol.com/tech-whitepaper.pdf',
+            ext: true,
+          },
+          {
+            id: 5,
+            name: 'Discord',
+            icon: require('@/assets/images/footer/discord_footer.svg'),
+            route: 'https://discord.gg/Yuawx9EP6e',
+            ext: true,
+          },
+        ],
       },
     ],
   }),
-
-  computed: {
-    footerDivider() {
-      return this.footerItems.find((el) => el.id === 'supreme')
-    },
-  },
 })
 </script>
 
