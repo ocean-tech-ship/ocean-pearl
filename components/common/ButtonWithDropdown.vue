01a0c3bbf20e0c4bb5fb4c1a91cf65439f1a4be3
moritzmurauer <moritzskaten@googlemail.com>
2021-06-09 12:01:01 +0200

    fix(NavbarDropdown): apply eslint standard

--- components/common/ButtonWithDropdown.vue
+++ components/common/ButtonWithDropdown.vue
@@ -2,70 +2,77 @@
   <main-dropdown>
     <template slot-scope="context">
       <img
-        @click="context.toggleOpen"
+        alt="treasure icon navbar"
         class="h-10 w-10 cursor-pointer rounded-full object-cover"
         src="@/assets/images/icons/treasure-card.svg"
-        alt="treasure icon navbar"
+        @click="context.toggleOpen"
+      />
+      <div
+        v-if="context.open"
+        class="shadow origin-top-right absolute right-0 mt-2 w-64 bg-grey border border-primary rounded overflow-hidden shadow-md"
       >
-        <div
-          v-if="context.open"
-          class="shadow origin-top-right absolute right-0 mt-2 w-64 bg-grey border border-primary rounded overflow-hidden shadow-md"
-        >
-          <ul @click="context.toggleOpen">
-            <li>
-              <NuxtLink class="font-bold block px-4 py-3" to="/">{{ $t('navbar.navbarLinkDefault') }}</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink class="font-bold block px-4 py-3" to="/projects">{{ $t('navbar.navbarProjects') }}</NuxtLink>
-            </li>  
-            <li>
-              <NuxtLink class="font-bold block px-4 py-3" to="/dao-project-overview">{{ $t('navbar.navbarDao') }}</NuxtLink>
-            </li>
-            <li>
-             <app-link to="https://github.com/oceanprotocol/oceandao/wiki/Grant-Proposal-Template">
+        <ul @click="context.toggleOpen">
+          <li>
+            <NuxtLink class="font-bold block px-4 py-3" to="/">{{
+              $t('navbar.navbarLinkDefault')
+            }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink class="font-bold block px-4 py-3" to="/projects">{{
+              $t('navbar.navbarProjects')
+            }}</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink
+              class="font-bold block px-4 py-3"
+              to="/dao-project-overview"
+              >{{ $t('navbar.navbarDao') }}</NuxtLink
+            >
+          </li>
+          <li>
+            <app-link
+              to="https://github.com/oceanprotocol/oceandao/wiki/Grant-Proposal-Template"
+            >
               <div class="flex px-4 py-3 text-primary">
                 <img
                   class="mr-2"
                   src="@/assets/images/icons/vote.svg"
-                  :alt="`${$t('project.proposal.submit')} ${$t('general.icon')}`"
+                  :alt="`${$t('project.proposal.submit')} ${$t(
+                    'general.icon'
+                  )}`"
                 />
-                <p>{{$t('project.proposal.submit')}}</p>
+                <p>{{ $t('project.proposal.submit') }}</p>
               </div>
-             </app-link>
-            </li>
-          </ul>
-        </div>
+            </app-link>
+          </li>
+        </ul>
+      </div>
     </template>
   </main-dropdown>
 </template>
 
 <script>
-import MainDropdown from "@/components/common/MainDropdown"
-import AppButton from '@/components/common/AppButton'
+import MainDropdown from '@/components/common/MainDropdown'
 import AppLink from '@/components/common/AppLink.vue'
 
 export default {
-  name: "button-with-dropdown",
-  data() {
-    return {
-      username: "John Wick",
-      email: "dontkillmydog@johnwick.com"
-    };
-  },
-  components: { 
+  name: 'ButtonWithDropdown',
+  components: {
     MainDropdown,
     AppLink,
-    AppButton
-  }
-};
+  },
+  data() {
+    return {
+      username: 'John Wick',
+      email: 'dontkillmydog@johnwick.com',
+    }
+  },
+}
 </script>
 
-
 <style scoped>
-
- li > a:hover {
-     background: #bb2c7636;
-     transition: 200ms;
- }
-
+li > a:hover {
+  background: #bb2c7636;
+  transition: 200ms;
+}
 </style>
