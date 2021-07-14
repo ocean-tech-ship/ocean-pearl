a1e42cb42c54a59bbee323e6bd8ee22c5e4d4f25
Marcel Haßlinger <me@marhali.de>
2021-07-14 15:07:02 +0200

    feat(middleware): do not show connect page if already connected

--- middleware/wallet.js
+++ middleware/wallet.js
@@ -1,6 +1,12 @@
-export default function ({ store, redirect }) {
-  // Redirect if wallet is not connected
+export default function ({ store, redirect, route }) {
+  // Redirect to connect page if wallet is not connected
   if (!store.state.wallet.connected) {
     redirect('/manage/connect')
+    return
+  }
+
+  // Do not show connect page if already connected
+  if (route.path === '/manage/connect') {
+    redirect('/manage')
   }
 }
