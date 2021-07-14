85fc0962a8de828974308539490a1611b06f064b
Marcel Haßlinger <me@marhali.de>
2021-07-14 15:07:54 +0200

    feat(pages): add wallet connect page

---
+++ pages/manage/connect/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <landing-section-container>
+    <div class="grid justify-center">
+      <h1>Login to manage your project or proposal</h1>
+      <app-button
+        text="Connect your Wallet"
+        @click="$store.dispatch('wallet/connect')"
+      />
+    </div>
+  </landing-section-container>
+</template>
+
+<script>
+import Vue from 'vue'
+import LandingSectionContainer from '@/components/app/landing/LandingSectionContainer'
+import AppButton from '@/components/common/AppButton'
+
+export default Vue.extend({
+  components: {
+    LandingSectionContainer,
+    AppButton,
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
