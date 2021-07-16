dc028852b119c9671cd0d8f567f0e722897d9230
moritzmurauer <moritzskaten@googlemail.com>
2021-07-16 16:06:50 +0200

    feat(DaoProjectsCategoreis): create new component for categories

---
+++ components/app/dao-project-overview/DaoProjectsCategories.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="flex flex-wrap text-primary">
+    <div class="flex border py-1 px-2 mr-1 mt-1">
+      <img
+        class="mr-1 p-1"
+        src="@/assets/images/icons/dropdown.svg"
+        alt="dropdown"
+      />
+      <p class="small-text pr-2">Round</p>
+    </div>
+    <div class="flex border py-1 px-2 mt-1">
+      <img
+        class="mr-1 p-1"
+        src="@/assets/images/icons/dropdown.svg"
+        alt="dropdown"
+      />
+      <p class="small-text">Category</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import EnumCategory from '../../enums/EnumCategory'
+
+export default {
+  data() {
+    return {
+      EnumCategory: {
+        [EnumCategory.All]: 'All',
+        [EnumCategory.DeFi]: 'DeFi',
+        [EnumCategory.Marketplace]: 'Marketplace',
+        [EnumCategory.Identity]: 'Identity',
+        [EnumCategory.Trading]: 'Trading',
+        [EnumCategory.Usage]: 'Usage',
+        [EnumCategory.BigData]: 'Big Data',
+        [EnumCategory.Ai]: 'AI',
+        [EnumCategory.Token]: 'Token',
+      },
+    }
+  },
+}
+</script>
+
+<style></style>
