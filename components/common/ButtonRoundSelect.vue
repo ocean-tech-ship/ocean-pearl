f07ec43490e99488a679473fbe54262f62fd7599
moritzmurauer <moritzskaten@googlemail.com>
2021-07-16 17:16:09 +0200

    feat(ButtonRoundSelect): create dropdown for round selection

---
+++ components/common/ButtonRoundSelect.vue
@@ -0,0 +1,51 @@
+<template>
+  <main-dropdown>
+    <template slot-scope="context">
+      <p class="small-text" @click="context.toggleOpen">select round</p>
+      <div
+        v-if="context.open"
+        class="shadow origin-top-right absolute left-0 mt-2 w-64 bg-grey border border-primary rounded overflow-hidden shadow-md"
+      >
+        <ul @click="context.toggleOpen">
+          <li v-for="(item, index) of items" :key="{ index }">
+            <NuxtLink class="small-text block px-4 py-3" to="/projects">
+              <p class="small-text">{{ item }}</p>
+            </NuxtLink>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </main-dropdown>
+</template>
+
+<script>
+import MainDropdown from '@/components/common/MainDropdown'
+
+export default {
+  name: 'ButtonWithDropdown',
+  components: {
+    MainDropdown,
+  },
+  props: {
+    items: {
+      type: Array,
+      default: () => [
+        'Round 7',
+        'Round 6',
+        'Round 5',
+        'Round 4',
+        'Round 3',
+        'Round 2',
+        'Round 1',
+      ],
+    },
+  },
+}
+</script>
+
+<style scoped>
+li > a:hover {
+  background: #bb2c7636;
+  transition: 200ms;
+}
+</style>
