250cabaa25ec7db1582d00ac5a46fceb97a6b2ab
moritzmurauer <moritzskaten@googlemail.com>
2021-07-16 16:06:33 +0200

    feat(DaoProjectsBadges): create new component for badges

---
+++ components/app/dao-project-overview/DaoProjectsBadges.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="grid space-y-1">
+    <div
+      v-for="Badge in EnumBadges"
+      :key="Badge"
+      class="justify-self-end flex items-center"
+    >
+      <p class="small-text text-right mr-2 text-primary">{{ Badge }}</p>
+      <img :src="BadgeIcons[Badge]" :alt="Badge" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import EnumBadges from '../../enums/EnumBadges'
+
+export default {
+  data() {
+    return {
+      EnumBadges: {
+        [EnumBadges.Featured]: 'General Grant',
+        [EnumBadges.Fund]: 'New Project',
+      },
+      BadgeIcons: {
+        'General Grant': require('@/assets/images/icons/featured-badge.svg'),
+        'New Project': require('@/assets/images/icons/bitcoin-badge.svg'),
+      },
+    }
+  },
+}
+</script>
