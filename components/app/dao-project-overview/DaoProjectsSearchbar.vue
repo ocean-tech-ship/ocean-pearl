8519306b3c69df08a7081167451a224bc3d196ed
moritzmurauer <moritzskaten@googlemail.com>
2021-07-16 16:07:30 +0200

    feat(DaoProjectsHeader): create new component for searchbar function

---
+++ components/app/dao-project-overview/DaoProjectsSearchbar.vue
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+<template>
+  <div class="space-x-4 flex hidden lg:flex">
+    <div>
+      <input
+        type="text"
+        placeholder="search project..."
+        class="py-3 pl-3 w-96 border-primary rounded border"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import EnumStatus from '../../enums/EnumStatus'
+
+export default {
+  data() {
+    return {
+      EnumStatus: {
+        [EnumStatus.All]: 'check',
+        [EnumStatus.Latest]: 'Latest',
+        [EnumStatus.Hiring]: 'Hiring',
+        [EnumStatus.Featured]: 'Featured',
+        [EnumStatus.Fund]: 'Awaiting Fund',
+      },
+    }
+  },
+}
+</script>
+
+<style></style>
