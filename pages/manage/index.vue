4d7276728910b6c55b2737b93c3c7a04edcc5645
Marcel Haßlinger <me@marhali.de>
2021-07-17 14:18:26 +0200

    feat(management page): add image update functionality

--- pages/manage/index.vue
+++ pages/manage/index.vue
@@ -51,19 +51,54 @@
       <div class="flex py-2">
         <div class="shadow rounded p-4 mr-4">
           <p class="text-primary small-text">Logo</p>
-          <app-logo
-            class="w-16 h-16 mx-4 inline-block rounded-full"
-            :src="project.logo"
-            :alt="project.title"
-          />
-        </div>
 
-        <div class="shadow rounded p-4">
-          <p class="text-primary small-text">Images</p>
+          <div class="flex items-center">
+            <app-logo
+              class="w-16 h-16 mx-4 inline-block rounded-full"
+              :src="project.logo"
+              :alt="project.title"
+            />
+
+            <div class="px-2">
+              <app-upload text="upload" @change="setLogo($event)" />
+            </div>
+
+            <div class="px-2">
+              <app-button text="delete" @click="project.logo = ''" />
+            </div>
+          </div>
         </div>
       </div>
 
-      <AppButton class="mt-4" text="Sign and save changes" @click="sign()" />
+      <div class="py-2">
+        <div class="shadow rounded p-4">
+          <p class="text-primary small-text">Images</p>
+
+          <div class="grid gap-4 grid-cols-1 md:grid-cols-2">
+            <div
+              v-for="picture in project.pictures"
+              :key="picture"
+              class="relative"
+            >
+              <div class="absolute w-full flex justify-end items-center">
+                <app-button text="X" @click="deleteImage(picture)" />
+              </div>
+
+              <img class="w-full h-64 rounded object-cover" :src="picture" />
+            </div>
+
+            <div
+              class="w-full h-64 border rounded border-primary flex justify-center items-center"
+            >
+              <app-upload text="add image" @change="addImages($event)" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex justify-center">
+        <AppButton class="mt-2" text="Sign and save changes" @click="sign()" />
+      </div>
     </section>
   </landing-section-container>
 </template>
@@ -72,15 +107,15 @@
 import Vue from 'vue'
 import LandingSectionContainer from '@/components/app/landing/LandingSectionContainer'
 import AppButton from '@/components/common/AppButton'
-import AppButtonStyle from '@/components/common/AppButtonStyle'
 import AppLogo from '@/components/common/AppLogo'
+import AppUpload from '@/components/common/AppUpload'
 
 export default Vue.extend({
   components: {
     LandingSectionContainer,
     AppButton,
-    AppButtonStyle,
     AppLogo,
+    AppUpload,
   },
 
   middleware: ['wallet'],
@@ -92,13 +127,35 @@ export default Vue.extend({
         description:
           'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
         category: 'buildAndIntegrate',
-        pictures: [],
+        pictures: [
+          'https://dataunion.app/wp-content/uploads/2020/11/skateboard.jpg',
+          'https://dataunion.app/wp-content/uploads/2020/11/winsome-girl-with-straight-hair-standing-on-bridge-with-camera.jpg',
+        ],
         logo: '',
-      }
+      },
     }
   },
 
   methods: {
+    deleteImage(image) {
+      const index = this.project.pictures.indexOf(image)
+      if (index > -1) {
+        this.project.pictures.splice(index, 1)
+      }
+    },
+
+    setLogo(event) {
+      if (event.target.files.length > 0) {
+        this.project.logo = URL.createObjectURL(event.target.files[0])
+      }
+    },
+
+    addImages(event) {
+      for (const file of event.target.files) {
+        this.project.pictures.push(URL.createObjectURL(file))
+      }
+    },
+
     async sign() {
       try {
         const sign = await this.$store.dispatch('wallet/signData', 'payload')
