196996f9744f0d15bcc0c1c56f0d7fc7facb0352
Marcel Haßlinger <me@marhali.de>
2021-07-17 14:13:22 +0200

    feat(components): add file upload component

---
+++ components/common/AppUpload.vue
@@ -0,0 +1,38 @@
+<template>
+  <label>
+    <div
+      class="select-none rounded font-medium py-2 px-6 shadow items-center justify-center"
+      :class="[
+        secondary
+          ? 'bg-white text-primary border-2 border-primary'
+          : 'call-to-action text-secondary',
+      ]"
+    >
+      <span>{{ text }}</span>
+    </div>
+    <input type="file" class="hidden" @change="$emit('change', $event)" />
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'AppUpload',
+
+  props: {
+    text: {
+      type: String,
+      default: '',
+    },
+    secondary: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.call-to-action {
+  background: linear-gradient(to right, #bb2c75 1%, #ff98cd);
+}
+</style>
