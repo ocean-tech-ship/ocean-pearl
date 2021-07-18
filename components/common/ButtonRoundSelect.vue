1b739965e821b2fd8e20d94781e86477bbbe0826
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2021-07-18 14:53:46 +0200

    fix(ButtonRoundSelect): fix index in key of dropdown options, change button name

--- components/common/ButtonRoundSelect.vue
+++ components/common/ButtonRoundSelect.vue
@@ -1,13 +1,13 @@
 <template>
   <main-dropdown>
     <template slot-scope="context">
-      <p class="small-text" @click="context.toggleOpen">select round</p>
+      <p class="small-text" @click="context.toggleOpen">Select Round</p>
       <div
         v-if="context.open"
         class="shadow origin-top-right absolute left-0 mt-2 w-64 bg-grey border border-primary rounded overflow-hidden shadow-md"
       >
         <ul @click="context.toggleOpen">
-          <li v-for="(item, index) of items" :key="{ index }">
+          <li v-for="(item, index) of items" :key="index">
             <NuxtLink class="small-text block px-4 py-3" to="/projects">
               <p class="small-text">{{ item }}</p>
             </NuxtLink>
