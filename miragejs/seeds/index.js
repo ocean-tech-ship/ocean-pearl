26c517648e0d7679cdb3ebd2b8ea3687d89efc29
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-28 22:01:54 +0200

    refactor(miragejs): add seed for rounds, format code

--- miragejs/seeds/index.js
+++ miragejs/seeds/index.js
@@ -2,13 +2,12 @@
  * Mirage JS guide on Seeds: https://miragejs.com/docs/data-layer/factories#in-development
  */
 
-const projectsSeeder = server => {
-    server.createList('project', 15),
-    server.createList('job', 12),
-    server.createList('daoproposal', 5)
-  };
-  
-  export default function seeds(server) {
+const projectsSeeder = async (server) => {
+    await server.createList('round', 7);
+    await server.createList('project', 15);
+};
+
+export default function seeds(server) {
     //server.loadFixtures();
     projectsSeeder(server);
-  }
+}
