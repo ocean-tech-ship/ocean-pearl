b839fc5949297f922f215e237916739b07479d44
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-28 21:50:46 +0200

    refactor(components): rename the job-overviews folder to match the naming pattern

--- components/app/jobs/JobBadges.vue
+++ components/app/jobs/JobBadges.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="grid w-64 space-y-1">
-    <div v-for="Badge in EnumBadges" :key="Badge" class="justify-self-end flex items-center">
+    <div v-for="Badge in BadgesEnum" :key="Badge" class="justify-self-end flex items-center">
       <p class="small-text  text-right mr-2 text-primary">{{Badge}}</p>
       <img :src="BadgeIcons[Badge]" :alt="Badge">
     </div>
   </div>
 </template>
 
-<script lang="ts">
-  import EnumBadges from '../../enums/EnumBadges';
+<script>
+  import BadgesEnum from '../../enums/Badges.enum';
 
   export default {
     data() {
       return {
-        EnumBadges: {
-          [EnumBadges.Featured]: 'Featured',
-          [EnumBadges.Fund]: 'Needs Funding'
+        BadgesEnum: {
+          [BadgesEnum.Featured]: 'Featured',
+          [BadgesEnum.Fund]: 'Needs Funding'
         },
          BadgeIcons: {
           'Featured': require('@/assets/images/icons/featured-badge.svg'),
---
+++ components/app/jobs/JobCategories.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="space-x-4 flex flex-wrap mx-56 hidden xl:flex">
+        <div v-for="category in CategoryEnum" :key="category" class="border text-primary py-1 px-2 my-2 flex">
+          <p class="small-text">{{category}}</p>
+        </div> 
+  </div>
+</template>
+
+<script>
+import CategoryEnum from '../../enums/Category.enum';
+
+  export default {
+    data() {
+      return {
+        CategoryEnum: {
+          [CategoryEnum.All]: 'All',
+          [CategoryEnum.DeFi]: 'DeFi',
+          [CategoryEnum.Economy]: 'Economy',
+          [CategoryEnum.Marketplace]: 'Marketplace',
+          [CategoryEnum.Identity]: 'Identity',
+          [CategoryEnum.Trading]: 'Trading',
+          [CategoryEnum.Usage]: 'Usage',
+          [CategoryEnum.BigData]: 'Big Data',
+          [CategoryEnum.Ai]: 'AI',
+          [CategoryEnum.Token]: 'Token'
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
