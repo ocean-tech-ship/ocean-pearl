526280b7283909adab4cb83c51d24bf29dafcdb8
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-28 21:42:58 +0200

    feat(global): rename and update enums

---
+++ components/enums/DaoProposalStatus.enum.ts
@@ -0,0 +1,12 @@
+enum DaoProposalStatusEnum {
+    Running = 'running',
+    Funded = 'funded',
+    NotFunded = 'notFunded',
+    Granted = 'granted',
+    NotGranted = 'notGranted',
+    Received = 'received',
+    Rejected = 'rejected',
+    Ended = 'ended',
+}
+
+export default DaoProposalStatusEnum
