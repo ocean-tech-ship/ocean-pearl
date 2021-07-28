21f67eaef0a21958410e91ce05a6aa19d7c66f16
Marcel Haßlinger <me@marhali.de>
2021-07-28 15:04:16 +0200

    fix(middleware): avoid endless redirect loop

--- middleware/session.js
+++ middleware/session.js
@@ -4,13 +4,13 @@ export default function ({ app, redirect, route }) {
   const cookie = app.$cookies.get(SESSION_COOKIE)
 
   // Redirect to login page if not logged in
-  if (!cookie) {
+  if (!cookie && route.path !== '/manage/login') {
     redirect('/manage/login')
     return
   }
 
   // Do not show login page if already authenticated
-  if (route.path === '/manage/login') {
+  if (!!cookie && route.path === '/manage/login') {
     redirect('/manage')
   }
 }
