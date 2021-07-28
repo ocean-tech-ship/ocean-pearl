cbfd12fa23eb2963709fea6d0f4724ffec27abad
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-28 22:04:23 +0200

    feat(miragejs): expand the factories of all models with extra values

--- miragejs/factories/job.js
+++ miragejs/factories/job.js
@@ -4,20 +4,19 @@
 import { Factory } from 'miragejs'
 
 export default {
-  job: Factory.extend({
-    _id(i) {
-      return i
-    },
-    title(i) {
-      return `Job ${i + 1}` // Project 1, Project 2, etc.
-    },
-    description:
-      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.',
-    position: 'Data Analyst',
-    website: 'https://oceanprotocol.com/',
-    logo: require('@/assets/images/poseidon-network.png'),
-    pictures: ['@/assets/images/poseidon-cover.png'],
-    salary: '50000€ - 60000€',
-    company: 'Company',
-  }),
+    job: Factory.extend({
+        id(i) {
+            return `Job${i}`
+        },
+        title(i) {
+            return `Job ${i + 1}`
+        },
+        description:
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.',
+        position: 'Data Analyst',
+        website: 'https://oceanprotocol.com/',
+        logo: require('@/assets/images/poseidon-network.png'),
+        pictures: ['@/assets/images/poseidon-cover.png'],
+        salary: '50000€ - 60000€',
+    }),
 }
