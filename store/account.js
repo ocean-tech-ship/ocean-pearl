69fa02b6eaadb4be2a5e7629804c0cc05933451a
Marcel Haßlinger <me@marhali.de>
2021-07-30 14:50:32 +0200

    feat(account): add action to retrieve account data

--- store/account.js
+++ store/account.js
@@ -1,5 +1,57 @@
-export const state = () => ({})
+import { getAccount } from 'api'
 
-export const mutations = {}
+export const state = () => ({
+  info: null,
+  error: null,
+  projects: null,
+})
 
-export const actions = {}
+export const mutations = {
+  info(state, payload) {
+    state.info = payload
+  },
+
+  error(state, payload) {
+    state.error = payload
+  },
+
+  projects(state, payload) {
+    state.projects = payload
+  },
+}
+
+export const actions = {
+  async loadAccount({ commit }) {
+    // Reset
+    commit('info', null)
+    commit('error', null)
+
+    try {
+      const response = await getAccount(this.$axios)
+      commit('projects', response.data)
+    } catch (error) {
+      if (error.response) {
+        const status = error.response.status
+
+        if (status === 401) {
+          // Authentication failure / timeout
+          commit('auth/message', 'Authentication timeout. Please try again.', {
+            root: true,
+          })
+          await this.$router.push('/manage/login')
+          return
+        }
+
+        if (status === 404) {
+          // No projects available for this account
+          commit('projects', [])
+          commit('info', 'There is no project for this account yet.')
+          return
+        }
+
+        commit('error', 'An error occurred. Please try again later.')
+        console.error('Error on backend communication', error)
+      }
+    }
+  },
+}
