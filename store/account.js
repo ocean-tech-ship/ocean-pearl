19d9c231ec8713a7a4b590d478a9d180567206a9
Marcel Haßlinger <me@marhali.de>
2021-07-30 16:36:22 +0200

    feat(account): add mockup data

--- store/account.js
+++ store/account.js
@@ -26,6 +26,36 @@ export const actions = {
     commit('info', null)
     commit('error', null)
 
+    // test mockup
+    function wait(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms))
+    }
+
+    await wait(1800)
+    commit('projects', [
+      {
+        id: '1',
+        title: 'Example Project',
+        description:
+          'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        category: 'buildAndIntegrate',
+        pictures: [
+          'https://dataunion.app/wp-content/uploads/2020/11/skateboard.jpg',
+          'https://dataunion.app/wp-content/uploads/2020/11/winsome-girl-with-straight-hair-standing-on-bridge-with-camera.jpg',
+        ],
+        logo: '',
+      },
+      {
+        id: '2',
+        title: 'Another really super cool project with long title',
+      },
+    ])
+
+    if (console) {
+      return
+    }
+    // end test mockup
+
     try {
       const response = await getAccount(this.$axios)
       commit('projects', response.data)
