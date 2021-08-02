a2b3a4922784596f604b0b2eec48bad83f437600
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-02 02:10:26 +0200

    feat(components): add a startDate to the metrics model

--- components/models/Metrics.model.ts
+++ components/models/Metrics.model.ts
@@ -1,9 +1,10 @@
 export interface Metrics {
     fundingRound: number;
     totalDaoProposals: number;
-    endDate: Date;
     startDate: Date;
     submissionEndDate: Date;
+    votingStartDate: Date;
+    endDate: Date;
     totalRequestedFunding: number;
     totalVotes: number;
 }
