cbf69b08c5be0aa8dddefdf9286cb9bf53d57485
Marcel Haßlinger <me@marhali.de>
2021-08-03 11:00:33 +0200

    feat(ProjectLogo): add error handling and upload functionality

--- components/app/account/project/ProjectLogo.vue
+++ components/app/account/project/ProjectLogo.vue
@@ -7,8 +7,8 @@
     <div class="flex flex-wrap items-center">
       <div class="w-16 h-16 mr-4">
         <img
-          v-if="!!project.logo"
-          :src="project.logo"
+          v-if="!!logo"
+          :src="logo"
           :alt="$t('general.logo')"
           class="w-16 h-16 inline-block rounded-full"
         />
@@ -16,11 +16,21 @@
 
       <div class="flex">
         <div class="mx-2">
-          <app-button :text="$t('general.upload')" />
+          <app-upload
+            secondary
+            :text="$t('general.upload')"
+            :accept="LOGO_ALLOWED_TYPES"
+            @change="uploadLogo($event)"
+          />
         </div>
 
         <div class="mx-2">
-          <app-button secondary :text="$t('general.delete')" />
+          <app-button
+            class="text-third border-none shadow-none"
+            secondary
+            :text="$t('general.delete')"
+            @click="deleteLogo()"
+          />
         </div>
       </div>
     </div>
@@ -28,20 +38,81 @@
 </template>
 
 <script>
+import Constants from '@/mixins/Constants'
+import AppUpload from '@/components/common/AppUpload'
 import AppButton from '@/components/common/AppButton'
 
 export default {
   name: 'ProjectLogo',
 
   components: {
+    AppUpload,
     AppButton,
   },
+
+  mixins: [Constants],
 
   props: {
     project: {
       type: Object,
       required: true,
       default: () => {},
+    },
+  },
+
+  data() {
+    return {
+      newLogo: undefined,
+    }
+  },
+
+  computed: {
+    logo() {
+      if (this.newLogo) {
+        return this.newLogo
+      }
+
+      // Logo has been deleted or fallback to previous logo
+      return this.newLogo === null ? null : this.$props.project.logo
+    },
+  },
+
+  methods: {
+    deleteLogo() {
+      this.newLogo && URL.revokeObjectURL(this.newLogo)
+      this.newLogo = null
+      this.$emit('change', null)
+    },
+
+    uploadLogo(handler) {
+      if (handler.target.files.length !== 1) {
+        return
+      }
+
+      const file = handler.target.files[0]
+
+      if (!this.LOGO_ALLOWED_TYPES.includes(file.type)) {
+        this.$store.commit(
+          'account/error',
+          this.$t('general.error.upload.type', {
+            types: this.LOGO_ALLOWED_TYPES,
+          })
+        )
+        return
+      }
+
+      if (file.size > this.LOGO_MAX_SIZE) {
+        this.$store.commit(
+          'account/error',
+          this.$t('general.error.upload.size', {
+            size: `${this.LOGO_MAX_SIZE / 1000}KB`,
+          })
+        )
+        return
+      }
+
+      this.newLogo = URL.createObjectURL(file)
+      this.$emit('change', file)
     },
   },
 }
---
+++ mixins/Constants.js
@@ -0,0 +1,10 @@
+export default {
+  data() {
+    return {
+      LOGO_MAX_SIZE: 500000, // 500KB
+      LOGO_ALLOWED_TYPES: 'image/jpeg,image/svg+xml',
+      IMAGE_MAX_SIZE: 4000000, // 4MB
+      IMAGE_ALLOWED_TYPES: 'image/jpeg',
+    }
+  },
+}
