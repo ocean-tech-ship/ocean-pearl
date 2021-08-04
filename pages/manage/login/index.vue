aadb8588827a8791f1933db85b33647c500f82ec
Marcel Haßlinger <me@marhali.de>
2021-08-04 16:21:48 +0200

    feat(auth): optimize login page design

--- pages/manage/login/index.vue
+++ pages/manage/login/index.vue
@@ -1,14 +1,73 @@
 <template>
   <landing-section-container>
-    <div class="grid justify-center">
-      <h1>Login to manage your project or proposal</h1>
+    <div class="flex justify-center pb-8">
+      <h2>Login here to manage your projects</h2>
+    </div>
 
-      <p v-if="!!$store.state.auth.message">
-        {{ $store.state.auth.message }}
-      </p>
+    <h4>
+      {{ $t('manage.auth.login.subtitle[0]')
+      }}<span class="text-primary">{{
+        $t('manage.auth.login.subtitle[1]')
+      }}</span>
+    </h4>
 
+    <div v-if="!!$store.state.auth.message" class="shadow rounded p-4 my-4">
+      {{ $store.state.auth.message }}
+    </div>
+
+    <div class="grid gap-8 lg:grid-cols-3 py-4">
+      <!-- choose your wallet provider -->
+      <div class="shadow rounded">
+        <div class="flex justify-center">
+          <img
+            src="@/assets/images/account/login/step-1.svg"
+            alt=""
+            class="h-64 w-64 object-contain"
+          />
+        </div>
+
+        <p class="p-4 font-bold">
+          <span class="text-primary">1.</span>
+          Choose your wallet provider
+        </p>
+      </div>
+
+      <!-- select account to connect -->
+      <div class="shadow rounded">
+        <div class="flex justify-center">
+          <img
+            src="@/assets/images/account/login/step-2.svg"
+            alt=""
+            class="h-64 w-64 object-contain"
+          />
+        </div>
+
+        <p class="p-4 font-bold">
+          <span class="text-primary">2.</span>
+          Select account to connect
+        </p>
+      </div>
+
+      <!-- sign login request -->
+      <div class="shadow rounded">
+        <div class="flex justify-center">
+          <img
+            src="@/assets/images/account/login/step-3.svg"
+            alt=""
+            class="h-64 w-64 object-contain"
+          />
+        </div>
+
+        <p class="p-4 font-bold">
+          <span class="text-primary">3.</span>
+          Sign login request
+        </p>
+      </div>
+    </div>
+
+    <div class="flex justify-center">
       <app-button
-        text="Login via Wallet"
+        :text="$t('manage.auth.login.action')"
         @click="$store.dispatch('auth/login')"
       />
     </div>
