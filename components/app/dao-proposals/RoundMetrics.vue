bc5a8341e877ac8670847446f1b38ac46e4eb1c3
Marcel Haßlinger <me@marhali.de>
2021-08-10 16:41:09 +0200

    style(RoundMetrics): apply eslint fix

--- components/app/dao-proposals/RoundMetrics.vue
+++ components/app/dao-proposals/RoundMetrics.vue
@@ -1,34 +1,41 @@
 <template>
-  <LandingSectionContainer>
-    <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 gap-4 mt-10"
-    >
-      <div v-for="card in daoInfoCards" :key="card.title">
-        <div class="shadow rounded p-4 pb-4">
-          <div class="flex align-center">
-            <div class="mr-3">
-              <img
-                class="inline-block mt-3 h-6 w-6"
-                :src="card.imageURL"
-                :alt="card.title"
-              />
+    <LandingSectionContainer>
+        <div
+            class="
+                grid grid-cols-1
+                md:grid-cols-2
+                lg:grid-cols-3
+                2xl:grid-cols-5
+                gap-4
+                mt-10
+            "
+        >
+            <div v-for="card in daoInfoCards" :key="card.title">
+                <div class="shadow rounded p-4 pb-4">
+                    <div class="flex align-center">
+                        <div class="mr-3">
+                            <img
+                                class="inline-block mt-3 h-6 w-6"
+                                :src="card.imageURL"
+                                :alt="card.title"
+                            />
+                        </div>
+                        <div>
+                            <p class="text-primary small-text line-clamp-1">
+                                {{ card.title }}
+                            </p>
+                            <p class="small-text">{{ card.daoInfo }}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div>
-              <p class="text-primary small-text line-clamp-1">
-                {{ card.title }}
-              </p>
-              <p class="small-text">{{ card.daoInfo }}</p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-    <hr class="text-primary my-16" />
-  </LandingSectionContainer>
+        <hr class="text-primary my-16" />
+    </LandingSectionContainer>
 </template>
 
 <script>
-import LandingSectionContainer from '../landing/LandingSectionContainer'
+import LandingSectionContainer from '@/components/app/landing/LandingSectionContainer';
 
 export default {
     name: 'RoundMetrics',
@@ -36,12 +43,12 @@ export default {
     components: {
         LandingSectionContainer,
     },
-  
+
     props: {
         metrics: {
             type: Object,
             required: true,
-            default: {
+            default: () => ({
                 fundingRound: '',
                 totalDaoProposals: '',
                 currentRound: {
@@ -58,7 +65,7 @@ export default {
                 },
                 totalRequestedFundingOcean: '',
                 totalVotes: '',
-            }
+            }),
         },
     },
 
@@ -66,49 +73,56 @@ export default {
         daoInfoCards() {
             return [
                 {
-                title: this.$t('daoRoundData.fundingRound'),
-                daoInfo: this.$t('project.proposal.round.numbered', {
-                    round: this.$props.metrics.fundingRound,
-                }),
-                imageURL: require('@/assets/images/icons/fund-round.svg'),
+                    title: this.$t('daoRoundData.fundingRound'),
+                    daoInfo: this.$t('project.proposal.round.numbered', {
+                        round: this.$props.metrics.fundingRound,
+                    }),
+                    imageURL: require('@/assets/images/icons/fund-round.svg'),
                 },
                 {
-                title: this.$t('daoRoundData.amountProposals'),
-                daoInfo: this.$t('daoRoundData.numberedProjects', {
-                    projects: this.$props.metrics.totalDaoProposals,
-                }),
-                imageURL: require('@/assets/images/icons/amount.svg'),
+                    title: this.$t('daoRoundData.amountProposals'),
+                    daoInfo: this.$t('daoRoundData.numberedProjects', {
+                        projects: this.$props.metrics.totalDaoProposals,
+                    }),
+                    imageURL: require('@/assets/images/icons/amount.svg'),
                 },
                 {
-                title: this.$i18n.t(
-                    this.$dateFns.isPast(new Date(this.metrics.currentRound.endDate))
-                    ? 'daoRoundData.countdown.after'
-                    : 'daoRoundData.countdown.before'
-                ),
-                daoInfo: this.$dateFns.formatDistanceToNowStrict(
-                    new Date(this.$props.metrics.currentRound.endDate),
-                    { locale: this.$i18n.locale, addSuffix: true }
-                ),
-                imageURL: require('@/assets/images/icons/countdown.svg'),
+                    title: this.$i18n.t(
+                        this.$dateFns.isPast(
+                            new Date(this.metrics.currentRound.endDate),
+                        )
+                            ? 'daoRoundData.countdown.after'
+                            : 'daoRoundData.countdown.before',
+                    ),
+                    daoInfo: this.$dateFns.formatDistanceToNowStrict(
+                        new Date(this.$props.metrics.currentRound.endDate),
+                        { locale: this.$i18n.locale, addSuffix: true },
+                    ),
+                    imageURL: require('@/assets/images/icons/countdown.svg'),
                 },
                 {
-                title: this.$t('daoRoundData.totalAmount'),
-                daoInfo: this.$props.metrics.paymentOption === 'ocean' 
-                    ? this.$t('general.ocean', {
-                    ocean: this.$props.metrics.totalRequestedFundingOcean,})
-                    : this.$t('general.usd', {
-                    usd: this.$props.metrics.totalRequestedFundingUsd,}),
-                imageURL: require('@/assets/images/icons/transaction.svg'),
+                    title: this.$t('daoRoundData.totalAmount'),
+                    daoInfo:
+                        this.$props.metrics.paymentOption === 'ocean'
+                            ? this.$t('general.ocean', {
+                                  ocean: this.$props.metrics
+                                      .totalRequestedFundingOcean,
+                              })
+                            : this.$t('general.usd', {
+                                  usd: this.$props.metrics
+                                      .totalRequestedFundingUsd,
+                              }),
+                    imageURL: require('@/assets/images/icons/transaction.svg'),
                 },
                 {
-                title: this.$t('daoRoundData.totalVotes'),
-                daoInfo: this.$t('general.ocean', {
-                    ocean: this.$props.metrics.totalVotes,
-                }),
-                imageURL: require('@/assets/images/icons/vote.svg'),
+                    title: this.$t('daoRoundData.totalVotes'),
+                    daoInfo: this.$t('general.ocean', {
+                        ocean: this.$props.metrics.totalVotes,
+                    }),
+                    imageURL: require('@/assets/images/icons/vote.svg'),
                 },
-            ]
+            ];
         },
     },
-}
+};
 </script>
