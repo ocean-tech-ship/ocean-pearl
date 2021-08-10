f60b9afd5f155871bd5b6ae897ae6353efbe0461
Marcel Haßlinger <me@marhali.de>
2021-08-10 21:53:34 +0200

    feat(components): add toolbar component for creating hint texts

---
+++ components/common/AppTooltip.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="tooltip">
+    <span v-if="!!hint" class="tooltip-hint rounded shadow p-1 bg-grey -mt-8">
+      {{ hint }}
+    </span>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppTooltip',
+
+  props: {
+    hint: {
+      type: String,
+      required: true,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style scoped>
+@tailwind base;
+@tailwind components;
+
+.tooltip-hint {
+  @apply invisible absolute;
+}
+
+.tooltip:hover .tooltip-hint {
+  @apply visible z-50;
+}
+
+@tailwind utilities;
+</style>
