e9bf23cbce228341e44591eb8d4c2ecffef74091
Marcel Haßlinger <me@marhali.de>
2021-08-10 21:55:51 +0200

    feat(RoundMetrics): display tooltip hint on countdowns

--- components/app/dao-proposals/RoundMetrics.vue
+++ components/app/dao-proposals/RoundMetrics.vue
@@ -1,33 +1,26 @@
 <template>
   <LandingSectionContainer>
-    <div
-      class="
-        grid grid-cols-1
-        md:grid-cols-2
-        lg:grid-cols-3
-        2xl:grid-cols-5
-        gap-4
-        mt-10
-      "
-    >
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
       <div v-for="card in daoInfoCards" :key="card.title">
-        <div class="shadow rounded p-4 pb-4">
-          <div class="flex align-center">
-            <div class="mr-3">
-              <img
-                class="inline-block mt-3 h-6 w-6"
-                :src="card.imageURL"
-                :alt="card.title"
-              />
-            </div>
-            <div>
-              <p class="text-primary small-text line-clamp-1">
-                {{ card.title }}
-              </p>
-              <p class="small-text">{{ card.daoInfo }}</p>
+        <app-tooltip :hint="card.tooltip">
+          <div class="shadow rounded p-4 pb-4">
+            <div class="flex align-center">
+              <div class="mr-3">
+                <img
+                  class="inline-block mt-3 h-6 w-6"
+                  :src="card.imageURL"
+                  :alt="card.title"
+                />
+              </div>
+              <div>
+                <p class="text-primary small-text line-clamp-1">
+                  {{ card.title }}
+                </p>
+                <p class="small-text">{{ card.daoInfo }}</p>
+              </div>
             </div>
           </div>
-        </div>
+        </app-tooltip>
       </div>
     </div>
     <hr class="text-primary my-16" />
@@ -36,12 +29,14 @@
 
 <script>
 import LandingSectionContainer from '@/components/app/landing/LandingSectionContainer';
+import AppTooltip from '@/components/common/AppTooltip';
 
 export default {
   name: 'RoundMetrics',
 
   components: {
     LandingSectionContainer,
+    AppTooltip,
   },
 
   props: {
@@ -93,6 +88,14 @@ export default {
             { locale: this.$i18n.locale, addSuffix: true },
           ),
           imageURL: require('@/assets/images/icons/rocket.svg'),
+          tooltip:
+            this.getSubmissionState().tooltip &&
+            this.$i18n.t(this.getSubmissionState().tooltip, {
+              timestamp: this.$dateFns.format(
+                this.getSubmissionState().timestamp,
+                'yyyy-MM-dd, hh:mm',
+              ),
+            }),
         },
         {
           title: this.$i18n.t(this.getVoteState().key),
@@ -101,6 +104,14 @@ export default {
             { locale: this.$i18n.locale, addSuffix: true },
           ),
           imageURL: require('@/assets/images/icons/countdown.svg'),
+          tooltip:
+            this.getVoteState().tooltip &&
+            this.$i18n.t(this.getVoteState().tooltip, {
+              timestamp: this.$dateFns.format(
+                this.getVoteState().timestamp,
+                'yyyy-MM-dd, hh:mm',
+              ),
+            }),
         },
         {
           title: this.$t('daoRoundData.totalAmount'),
@@ -137,7 +148,8 @@ export default {
       if (now < startDate) {
         // Submission has not started yet
         return {
-          key: 'daoRoundData.submit.before',
+          key: 'daoRoundData.submit.before.title',
+          tooltip: 'daoRoundData.submit.before.tooltip',
           timestamp: startDate,
         };
       }
@@ -145,7 +157,8 @@ export default {
       if (now < submissionEndDate) {
         // Submission is active
         return {
-          key: 'daoRoundData.submit.active',
+          key: 'daoRoundData.submit.active.title',
+          tooltip: 'daoRoundData.submit.active.tooltip',
           timestamp: submissionEndDate,
         };
       }
@@ -154,7 +167,8 @@ export default {
         // Round ended. Show next round if possible
         if (metrics.nextRound && metrics.nextRound.startDate) {
           return {
-            key: 'daoRoundData.submit.next',
+            key: 'daoRoundData.submit.next.title',
+            tooltip: 'daoRoundData.submit.next.tooltip',
             timestamp: new Date(metrics.nextRound.startDate),
           };
         }
@@ -162,7 +176,8 @@ export default {
 
       // Submission ended
       return {
-        key: 'daoRoundData.submit.after',
+        key: 'daoRoundData.submit.after.title',
+        tooltip: 'daoRoundData.submit.after.tooltip',
         timestamp: submissionEndDate,
       };
     },
@@ -176,7 +191,8 @@ export default {
       if (now < votingStartDate) {
         // Voting has not started yet
         return {
-          key: 'daoRoundData.vote.before',
+          key: 'daoRoundData.vote.before.title',
+          tooltip: 'daoRoundData.vote.before.tooltip',
           timestamp: votingStartDate,
         };
       }
@@ -184,7 +200,8 @@ export default {
       if (now < endDate) {
         // Voting is active
         return {
-          key: 'daoRoundData.vote.active',
+          key: 'daoRoundData.vote.active.title',
+          tooltip: 'daoRoundData.vote.active.tooltip',
           timestamp: endDate,
         };
       }
@@ -192,14 +209,16 @@ export default {
       if (metrics.nextRound && metrics.nextRound.votingStartDate) {
         // Voting ended. Show next round if possible
         return {
-          key: 'daoRoundData.vote.next',
+          key: 'daoRoundData.vote.next.title',
+          tooltip: 'daoRoundData.vote.next.tooltip',
           timestamp: new Date(metrics.nextRound.votingStartDate),
         };
       }
 
       // Voting ended
       return {
-        key: 'daoRoundData.vote.after',
+        key: 'daoRoundData.vote.after.title',
+        tooltip: 'daoRoundData.vote.after.tooltip',
         timestamp: endDate,
       };
     },
