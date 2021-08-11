9742b156783eae76c64ddcc2b67d6703e215f8c6
Marcel Haßlinger <me@marhali.de>
2021-08-11 10:21:47 +0200

    feat(components): add jdenticon as identicon generator

---
+++ components/common/AppIdenticon.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg :width="size" :height="size" :data-jdenticon-value="value" />
+</template>
+
+<script>
+import { update } from 'jdenticon';
+
+export default {
+  name: 'AppIdenticon',
+
+  props: {
+    value: {
+      type: String,
+      required: true,
+      default: '',
+    },
+
+    size: {
+      type: Number,
+      required: false,
+      default: 64,
+    },
+  },
+
+  watch: {
+    value() {
+      update('[data-jdenticon-value]');
+    },
+  },
+
+  beforeMount() {
+    update('[data-jdenticon-value]');
+  },
+};
+</script>
+
+<style scoped></style>
