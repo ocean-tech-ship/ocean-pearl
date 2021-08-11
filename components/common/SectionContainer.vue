15c6d3233698d1d430e9952515594dbc94185e28
Marcel Haßlinger <me@marhali.de>
2021-08-11 16:49:50 +0200

    feat(components): add new container to replace legacy LandingSectionContainer

---
+++ components/common/SectionContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="px-1 2sm:px-4 sm:px-16 md:px-32 mx-auto max-w-1440">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SectionContainer',
+};
+</script>
+
+<style scoped></style>
