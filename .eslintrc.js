3feddd12a4fee279b7bfed812e783d55c81984eb
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2021-08-14 18:59:01 +0200

    feat(eslint): add airbnb-base rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        node: true,
-    },
-    extends: [
-        '@nuxtjs/eslint-config-typescript',
-        'plugin:nuxt/recommended',
-        'prettier',
-    ],
-    plugins: [],
-    // add your custom rules here
-    rules: {},
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+    'prettier',
+  ],
+  plugins: [],
+  // add your custom rules here
+  rules: {},
 };
