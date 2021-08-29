8907eceab10258f77be026e8e500c4f3cb596b1d
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2021-08-29 18:34:00 +0200

    feat(DaoProposalsResponse): refactor markup and lang object,integrate button

--- components/app/dao-proposals/DaoProposalsResponse.vue
+++ components/app/dao-proposals/DaoProposalsResponse.vue
@@ -1,18 +1,26 @@
 <template>
   <div
     v-if="!proposalsFound"
-    class="flex flex-col items-center max-w-sm px-4 md:px-0 mt-12 md:mt-16"
+    class="flex flex-col items-center px-4 md:px-0 mt-12 md:mt-16"
   >
     <img :src="notFoundImg.src" :alt="notFoundImg.alt" />
     <div v-if="!searchUsed" class="text-center mt-8">
       <h3 v-html="$t('dao-projects.filterResponse.noSearch.heading')" />
-      <p>
+      <p class="mt-1">
         {{ $t('dao-projects.filterResponse.noSearch.paragraph') }}
       </p>
+      <app-link
+        to="https://github.com/oceanprotocol/oceandao/wiki/Grant-Proposal-Template"
+      >
+        <app-button-style
+          class="text-center mt-4"
+          :text="$t('dao-projects.filterResponse.noSearch.button')"
+        />
+      </app-link>
     </div>
     <div v-if="searchUsed" class="text-center mt-8">
       <h3 class="" v-html="$t('dao-projects.filterResponse.search.heading')" />
-      <p>
+      <p class="mt-1">
         {{ $t('dao-projects.filterResponse.search.paragraph') }}
       </p>
     </div>
@@ -20,8 +28,12 @@
 </template>
 
 <script>
+import AppLink from '@/components/common/AppLink.vue';
+import AppButtonStyle from '@/components/common/AppButtonStyle.vue';
+
 export default {
   name: 'DaoProposalsResponse',
+  components: { AppLink, AppButtonStyle },
   props: {
     proposalsFound: {
       type: Boolean,
