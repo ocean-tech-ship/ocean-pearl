1cd13f37b768f7a587ddf07e52d7ecbcca57ffcb
Marcel Haßlinger <me@marhali.de>
2021-08-31 11:47:46 +0200

    fix(history): apply fixed width on round number

--- components/app/project-detail/ProjectSingleDaoProposalHistory.vue
+++ components/app/project-detail/ProjectSingleDaoProposalHistory.vue
@@ -49,10 +49,10 @@
                   : ''
               "
             >
-              <td class="text-left p-4 py-3 pl-4">
-                <span class="rounded border border-darkgrey py-1 px-2">
-                  {{ proposal.fundingRound.round }}
-                </span>
+              <td class="text-left p-4 py-3 pl-4 flex">
+                <div class="rounded border border-darkgrey w-10 text-center py-1 px-2">
+                  <span>{{ proposal.fundingRound.round }}</span>
+                </div>
               </td>
 
               <td class="text-right p-4 py-3">
@@ -81,7 +81,7 @@
                   <img
                     class="self-start pl-1"
                     src="@/assets/images/icons/ocean.svg"
-                    alt=""
+                    alt="$OCEAN"
                   />
                 </div>
 
@@ -119,7 +119,7 @@
 </template>
 
 <script>
-import AppLink from '@/components/common/AppLink';
+import AppLink from '@/components/common/AppLink.vue';
 
 export default {
   name: 'ProjectSingleDaoProposalHistory',
