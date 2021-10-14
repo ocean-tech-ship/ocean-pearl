4553b49513dc8f7c8c92acd7f55f14db3b3df66a
Marcel Haßlinger <me@marhali.de>
2021-10-14 15:11:11 +0200

    feat(leaderboard): outsource head data into dedicated file

--- pages/leaderboard/index.vue
+++ pages/leaderboard/index.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+import createHead from './index.head';
 import Vue from 'vue';
 import SectionContainer from '@/components/common/SectionContainer.vue';
 
@@ -12,44 +13,7 @@ export default Vue.extend({
   components: { SectionContainer },
 
   head() {
-    return {
-      title: this.$t('leaderboard.meta.title'),
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: this.$t('leaderboard.meta.description'),
-        },
-        {
-          hid: 'og:title',
-          property: 'og:title',
-          content: this.$t('leaderboard.meta.title'),
-        },
-        {
-          hid: 'og:description',
-          property: 'og:description',
-          content: this.$t('leaderboard.meta.description'),
-        },
-        {
-          hid: 'og:url',
-          property: 'og:url',
-          content: `${this.$config.rootURL}/leaderboard`,
-        },
-        {
-          hid: 'twitter:title',
-          property: 'twitter:title',
-          content: `${this.$t('leaderboard.meta.title')} | Ocean Pearl`,
-        },
-        {
-          hid: 'twitter:description',
-          property: 'twitter:description',
-          content: this.$t('leaderboard.meta.description'),
-        },
-      ],
-      link: [
-        { rel: 'canonical', href: `${this.$config.rootURL}/leaderboard` },
-      ]
-    }
+    return createHead(this.$config, this.$i18n)
   }
 });
 </script>
