179f6bb83317332b159d5c74129acc332c7e4119
Marcel Haßlinger <me@marhali.de>
2021-10-15 21:52:59 +0200

    feat(components): add primary background component

---
+++ components/common/AppPrimaryGradientBackground.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="background">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppPrimaryGradientBackground',
+};
+</script>
+
+<style scoped lang="scss">
+.background {
+  background: linear-gradient(to bottom right, #7b3070 5%, #d45b8d);
+}
+</style>
