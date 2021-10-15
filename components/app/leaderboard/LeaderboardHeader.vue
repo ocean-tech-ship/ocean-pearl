227ec078859ecb9d3af3cbddfb6585585ebb0830
Marcel Haßlinger <me@marhali.de>
2021-10-15 22:32:59 +0200

    feat(leaderboard): create header component

---
+++ components/app/leaderboard/LeaderboardHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>
+      {{ $t('leaderboard.title[0]') }}
+      <span class="text-primary">
+        {{ $t('leaderboard.title[1]') }}
+      </span>
+    </h2>
+
+    <p>{{ $t('leaderboard.subtitle') }}</p>
+
+    <!-- TODO: metrics -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LeaderboardHeader',
+};
+</script>
+
+<style scoped>
+
+</style>
