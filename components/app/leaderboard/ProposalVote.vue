02839d1f200b0d8ff7bb7dab36991388a63e37dc
Marcel Haßlinger <me@marhali.de>
2021-10-15 22:33:50 +0200

    feat(leaderboard): create vote action component

---
+++ components/app/leaderboard/ProposalVote.vue
@@ -0,0 +1,50 @@
+<template>
+  <app-link :to="proposal.voteUrl || ''">
+    <div
+      :class="{ 'bg-primary bg-opacity-10': background }"
+      class="
+      relative
+      rounded
+      w-12
+      h-12
+      flex
+      items-center
+      justify-center
+    "
+    >
+      <app-icon class="text-primary" :path="mdiVote" />
+    </div>
+  </app-link>
+</template>
+
+<script lang="ts">
+import { mdiVote } from '@mdi/js';
+import { LeaderboardProposal } from '@/models/Leaderboard.model';
+import AppIcon from '@/components/common/AppIcon.vue';
+import AppLink from '@/components/common/AppLink.vue';
+
+export default {
+  name: 'ProposalVote',
+
+  components: { AppLink, AppIcon },
+
+  props: {
+    proposal: {
+      type: Object as () => LeaderboardProposal,
+      required: true,
+    },
+
+    background: {
+      type: Boolean,
+      required: false,
+      default: false,
+    }
+  },
+
+  data() {
+    return {
+      mdiVote,
+    };
+  },
+};
+</script>
