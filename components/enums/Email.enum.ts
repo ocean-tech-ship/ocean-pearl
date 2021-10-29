7f59c5e8c15cdff88325a06ffef17608c0e7ddcf
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2021-10-29 12:21:24 +0200

    refactor(Email.enum): use template literals for pgp key

--- components/enums/Email.enum.ts
+++ components/enums/Email.enum.ts
@@ -1,21 +1,21 @@
 enum EmailEnum {
   Address = 'oceanpearlio@protonmail.com',
-  Pgp = '-----BEGIN PGP PUBLIC KEY BLOCK-----\n' +
-    'Version: OpenPGP.js v4.10.10\n' +
-    'Comment: https://openpgpjs.org\n' +
-    '\n' +
-    'xjMEYVTMZBYJKwYBBAHaRw8BAQdAfIdsbInurPwWXbqET2clW8kqEhgZoLce\n' +
-    'niLWZFQbA1PNOW9jZWFucGVhcmxpb0Bwcm90b25tYWlsLmNvbSA8b2NlYW5w\n' +
-    'ZWFybGlvQHByb3Rvbm1haWwuY29tPsKPBBAWCgAgBQJhVMxkBgsJBwgDAgQV\n' +
-    'CAoCBBYCAQACGQECGwMCHgEAIQkQqdqPFMv/HvoWIQSTNkA1HBiUACbVd3yp\n' +
-    '2o8Uy/8e+q3GAP9bALUw9u092pN/j+c0BEKaej9Q3g+iM/5V6/aD1GpRPwEA\n' +
-    'q4DWc4ETVflFGdTLZPG79Aquq/RTwDkpA6gXJQycjQnOOARhVMxkEgorBgEE\n' +
-    'AZdVAQUBAQdAIN1VniH9Pexs6EBEfh4+wY6EZgLLx8o2XSapzG0few4DAQgH\n' +
-    'wngEGBYIAAkFAmFUzGQCGwwAIQkQqdqPFMv/HvoWIQSTNkA1HBiUACbVd3yp\n' +
-    '2o8Uy/8e+nzjAQCVXNFxItsFeam9uSOesl+G3TFV4SAPMJM++ZaRpnBhAAD9\n' +
-    'ES6dVfDSTUi/MhnBJQwhsFO6WuShaVIg2lpzF/j2kgQ=\n' +
-    '=2OXe\n' +
-    '-----END PGP PUBLIC KEY BLOCK-----',
+  Pgp = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: OpenPGP.js v4.10.10
+Comment: https://openpgpjs.org
+
+xjMEYVTMZBYJKwYBBAHaRw8BAQdAfIdsbInurPwWXbqET2clW8kqEhgZoLce
+niLWZFQbA1PNOW9jZWFucGVhcmxpb0Bwcm90b25tYWlsLmNvbSA8b2NlYW5w
+ZWFybGlvQHByb3Rvbm1haWwuY29tPsKPBBAWCgAgBQJhVMxkBgsJBwgDAgQV
+CAoCBBYCAQACGQECGwMCHgEAIQkQqdqPFMv/HvoWIQSTNkA1HBiUACbVd3yp
+2o8Uy/8e+q3GAP9bALUw9u092pN/j+c0BEKaej9Q3g+iM/5V6/aD1GpRPwEA
+q4DWc4ETVflFGdTLZPG79Aquq/RTwDkpA6gXJQycjQnOOARhVMxkEgorBgEE
+AZdVAQUBAQdAIN1VniH9Pexs6EBEfh4+wY6EZgLLx8o2XSapzG0few4DAQgH
+wngEGBYIAAkFAmFUzGQCGwwAIQkQqdqPFMv/HvoWIQSTNkA1HBiUACbVd3yp
+2o8Uy/8e+nzjAQCVXNFxItsFeam9uSOesl+G3TFV4SAPMJM++ZaRpnBhAAD9
+ES6dVfDSTUi/MhnBJQwhsFO6WuShaVIg2lpzF/j2kgQ=
+=2OXe
+-----END PGP PUBLIC KEY BLOCK-----`,
 }
 
 export default EmailEnum;
