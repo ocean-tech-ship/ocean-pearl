9cb5214a15cd2f1bf693d8dbf814bb99ea79c5fe
Marcel Haßlinger <me@marhali.de>
2021-11-16 14:21:33 +0100

    feat(store): implement new alert component

--- store/auth.js
+++ store/auth.js
@@ -1,89 +1,83 @@
-import { login, logout } from '@/api'
+import { login, logout } from '@/api';
 
 export const SESSION_NAME = 'session-shadow';
 
 export const state = () => ({
   loggedInAddress: null,
-  message: null,
 });
 
 export const mutations = {
   loggedInAddress(state, payload) {
-    state.loggedInAddress = payload
-  },
-
-  message(state, payload) {
-    state.message = payload
+    state.loggedInAddress = payload;
   },
 };
 
 export const actions = {
   async login({ dispatch, commit }) {
-    commit('message', null) // Reset
-    let wallet, signature
+    commit('alert/clear', null, { root: true }); // Reset
+    let wallet, signature;
 
     try {
-      wallet = await dispatch('wallet/connect', null, { root: true })
+      wallet = await dispatch('wallet/connect', null, { root: true });
     } catch (error) {
-      console.error('Could not connect wallet', error)
-      commit('message', this.$i18n.t('manage.auth.error.wallet'))
-      return
+      console.error('Could not connect wallet', error);
+      dispatch('alert/error', 'manage.auth.error.wallet', { root: true });
+      return;
     }
 
-    const timestamp = new Date()
-    const doc = `oceanpearl.io - login @ ${timestamp.toISOString()}`
+    const timestamp = new Date();
+    const doc = `oceanpearl.io - login @ ${timestamp.toISOString()}`;
 
     try {
-      signature = await dispatch('wallet/signData', doc, { root: true })
+      signature = await dispatch('wallet/signData', doc, { root: true });
     } catch (error) {
-      console.error('Could not sign login request', error)
-      commit('message', this.$i18n.t('manage.auth.error.sign'))
-      return
+      console.error('Could not sign login request', error);
+      dispatch('alert/error', 'manage.auth.error.sign', { root: true });
+      return;
     }
 
     const payload = {
       wallet,
       timestamp,
       signature,
-    }
+    };
 
     try {
       const response = await login(this.$axios, payload);
 
       // Login was successful
       commit('loggedInAddress', response.data.wallet);
-      await this.$router.push('/management')
-      await dispatch('wallet/disconnect', null, { root: true })
-
+      await this.$router.push('/management');
+      await dispatch('wallet/disconnect', null, { root: true });
     } catch (error) {
       if (error.response && error.response.status === 401) {
-        commit('message', this.$i18n.t('manage.auth.error.invalid'))
-        return
+        dispatch('alert/error', 'manage.auth.error.invalid', { root: true });
+        return;
       }
 
-      console.error('Error on backend communication', error)
-      commit('message', this.$i18n.t('general.error.retry'))
+      console.error('Error on backend communication', error);
+      dispatch('alert/error', 'general.error.retry', { root: true });
     }
   },
 
-  async logout({ commit }) {
+  async logout({ commit, dispatch }) {
     try {
-      await logout(this.$axios)
+      await logout(this.$axios);
     } catch (error) {
-      console.error('Error on logging out', error)
+      console.error('Error on logging out', error);
     }
 
     // Logout was successful
-    commit('message', this.$i18n.t('manage.auth.logout.completed'))
-    await this.$router.push('/management/login')
+    dispatch('alert/success', 'manage.auth.logout.completed', { root: true });
+    await this.$router.push('/management/login');
 
     // Reset state
     commit('account/wallet', null, { root: true });
     commit('account/projects', null, { root: true });
   },
 
-  timeout({ commit }) {
-    commit('message', this.$i18n.t('manage.auth.timeout'));
+  timeout({ dispatch }) {
+    dispatch('alert/warning', 'manage.auth.timeout', { root: true });
     this.$cookies.remove(SESSION_NAME);
-  }
-}
+  },
+};
