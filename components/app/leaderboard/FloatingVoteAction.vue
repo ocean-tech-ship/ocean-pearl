1d381626149977ac0bad312a81f063d33a8ccfbc
marhali <me@marhali.de>
2021-11-22 22:14:21 +0100

    feat(leaderboard): optimize for mobile devices

--- components/app/leaderboard/FloatingVoteAction.vue
+++ components/app/leaderboard/FloatingVoteAction.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="sticky top-3/4 lg:top-1/2">
-    <div class="absolute right-0">
-      <app-link :to="voteUrl" :data-analytics="dataAnalytics">
-        <div class="bg-white rounded-l shadow p-4">
-          <div
-            class="
-              bg-primary bg-opacity-10
-              relative
-              rounded
-              w-12
-              lg:w-16
-              h-12
-              lg:h-16
-              flex
-              items-center
-              justify-center
-              transition
-              duration-300
-              ease-in-out
-              lg:hover:scale-125
-            "
-          >
-            <app-icon class="text-primary" :size="36" :data="icons.vote" />
-          </div>
+  <div
+    class="
+      fixed
+      w-full
+      flex
+      justify-end
+      lg:top-1/2
+      bottom-2
+      pointer-events-none
+    "
+  >
+    <app-link :to="voteUrl" :data-analytics="dataAnalytics">
+      <div class="bg-white rounded-l shadow p-4 pointer-events-auto">
+        <div
+          class="
+            bg-primary bg-opacity-10
+            relative
+            rounded
+            w-12
+            lg:w-16
+            h-12
+            lg:h-16
+            flex
+            items-center
+            justify-center
+            transition
+            duration-300
+            ease-in-out
+            lg:hover:scale-125
+          "
+        >
+          <app-icon class="text-primary" :size="36" :data="icons.vote" />
         </div>
-      </app-link>
-    </div>
+      </div>
+    </app-link>
   </div>
 </template>
 
