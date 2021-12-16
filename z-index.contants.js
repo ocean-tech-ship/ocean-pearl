7864a22e2e6875dd789eb6510d8b9acfad9eb3b4
Marcel Haßlinger <me@marhali.de>
2021-12-16 16:24:25 +0100

    feat(tailwind): introduce z-index policy constants

---
+++ z-index.contants.js
@@ -0,0 +1,29 @@
+/*
+ * Our z-index is based on the initial base value (ideally base = 0).
+ * All constants are build on top of the base value by adding the arithmetic above or below value.
+ */
+
+const base = 0;
+
+// z-index arithmetics
+const above = 1; // only for constants above base
+const below = -1; // only for constants below base
+
+const backdrop = below + base;
+const label = above + base;
+const dropdown = above + label;
+const nav = above + dropdown;
+const tooltip = above + nav;
+const popup = above + tooltip;
+const modal = above + popup;
+
+export default {
+  backdrop,
+  base,
+  label,
+  dropdown,
+  nav,
+  tooltip,
+  popup,
+  modal,
+};
