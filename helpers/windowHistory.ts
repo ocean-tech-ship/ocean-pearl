8979400a1b73738b35dff28d389caf79798e5740
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2022-01-21 21:56:55 +0100

    fix(windowHistory): use router for replacing query params

--- helpers/windowHistory.ts
+++ helpers/windowHistory.ts
@@ -2,13 +2,10 @@ import CategoryEnum from '@/enums/Category.enum';
 
 export default function replaceQueryParams(_this: any, query: object): void {
   if (!process.server) {
-    const url =
-      window.location.origin +
-      _this.$nuxt.$router.resolve({
-        query,
-      }).href;
-    // eslint-disable-next-line
-    history.replaceState({}, '', url);
+    _this.$nuxt.$router.push({
+      path: _this.$nuxt.$route.path,
+      query,
+    });
   }
 }
 
