4d06b2cc3b10dea6e1d960bea60afdb5ebdc7466
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2022-01-30 23:31:57 +0100

    feat(LeaderboardFilter): create filter and dropdown container

---
+++ components/app/leaderboard/LeaderboardDropdowns.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <button-emit-with-dropdown
+      button-name="Select Round"
+      class="rounded w-36 mt-10 border-white bg-white"
+      button-style="text-third"
+      label-style="-m-3 text-white"
+      :menu-items="roundItems"
+      @selected="setItems"
+    />
+  </div>
+</template>
+
+<script>
+import ButtonEmitWithDropdown from '@/components/common/ButtonEmitWithDropdown.vue';
+
+export default {
+  name: 'LeaderBoardDropdowns',
+
+  components: {
+    ButtonEmitWithDropdown,
+  },
+  props: {
+    rounds: {
+      type: Number,
+      required: true,
+    },
+    filter: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      watching: false,
+      roundItems: [
+        {
+          type: 'rounds',
+          content: `Current`,
+          id: 0,
+          selected: this.filter.round === 0,
+        },
+      ],
+      categoryItems: [],
+    };
+  },
+  watch: {
+    $data: {
+      deep: true,
+      handler: function emit() {
+        if (this.watching) {
+          const selectedItems = Object.values(
+            this.roundItems.reduce((accumulator, current) => {
+              if (current.selected) accumulator.push(current.id);
+              return accumulator;
+            }, []),
+          );
+
+          this.$emit('selected-items', {
+            round: selectedItems[0],
+          });
+        }
+      },
+    },
+  },
+  created() {
+    // set rounds button content
+    for (let i = this.rounds - 1; i >= 6; i--) {
+      this.roundItems = [
+        ...this.roundItems,
+        {
+          type: 'rounds',
+          content: `Round ${i}`,
+          id: i,
+          selected: this.filter.round === i,
+        },
+      ];
+    }
+  },
+  methods: {
+    setItems(payload) {
+      this.watching = true;
+
+      const { type, id } = payload;
+
+      switch (type) {
+        case 'rounds':
+          this.roundItems.forEach(
+            // eslint-disable-next-line no-param-reassign
+            (roundItem) => (roundItem.selected = roundItem.id === id),
+          );
+          break;
+        default:
+      }
+    },
+  },
+};
+</script>
---
+++ components/app/leaderboard/LeaderboardFilter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <leaderboard-dropdowns
+      :rounds="rounds"
+      :filter="filter"
+      @selected-items="setFilterAndFetch"
+    />
+  </div>
+</template>
+
+<script>
+import replaceQueryParams from '@/helpers/windowHistory.ts';
+import LeaderboardDropdowns from '~/components/app/leaderboard/LeaderboardDropdowns';
+
+export default {
+  name: 'LeaderboardFilter',
+
+  components: {
+    LeaderboardDropdowns,
+  },
+
+  props: {
+    rounds: {
+      type: Number,
+      required: true,
+    },
+    filter: {
+      type: Object,
+      required: true,
+    },
+    setFilter: {
+      type: Function,
+      required: true,
+    },
+    fetchLeaderboard: {
+      type: Function,
+      required: true,
+    },
+  },
+
+  methods: {
+    setFilterAndFetch(payload) {
+      const { round } = payload;
+
+      // set new filter
+      const newFilter = {
+        round: round || round === 0 ? round : this.filter.round,
+      };
+
+      this.setFilter(newFilter).then(() =>
+        this.fetchLeaderboard().then((query) =>
+          replaceQueryParams(this, query),
+        ),
+      );
+    },
+  },
+};
+</script>
