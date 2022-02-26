be73b0e6179ab34137530604c4e7bdb4befb7451
marhali <me@marhali.de>
2022-02-26 13:28:04 +0100

    refactor(LandingHeroSection): remove legacy section container

--- components/app/landing/LandingHeroSection.vue
+++ components/app/landing/LandingHeroSection.vue
@@ -1,39 +1,36 @@
 <template>
-  <LandingSectionContainer class="md:mb-32 xl:mb-64">
-    <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-8">
-      <div class="pt-6">
-        <h1 class="text-base-content">
-          {{ $t('landing.hero.titlePart1') }}
-          <span class="text-primary">
-            {{ $t('landing.hero.titleHighlight') }}
-          </span>
-          {{ $t('landing.hero.titlePart2') }}
-        </h1>
-        <p class="pt-6 mb-8 text-base-content">
-          {{ $t('landing.hero.text') }}
-        </p>
-        <AppLink to="/projects">
-          <app-button-style
-            :icon="icons.sailboat"
-            :text="$t('landing.hero.buttonText')"
-          />
-        </AppLink>
-      </div>
-      <div class="hidden md:flex">
-        <img
-          class="self-center w-full pt-8 lg:pt-0"
-          src="@/assets/images/landing/squid-full.svg"
-          alt="kraken"
+  <div class="w-full grid grid-cols-1 lg:grid-cols-2 gap-8">
+    <div class="pt-6">
+      <h1 class="text-base-content">
+        {{ $t('landing.hero.titlePart1') }}
+        <span class="text-primary">
+          {{ $t('landing.hero.titleHighlight') }}
+        </span>
+        {{ $t('landing.hero.titlePart2') }}
+      </h1>
+      <p class="pt-6 mb-8 text-base-content">
+        {{ $t('landing.hero.text') }}
+      </p>
+      <AppLink to="/projects">
+        <app-button-style
+          :icon="icons.sailboat"
+          :text="$t('landing.hero.buttonText')"
         />
-      </div>
+      </AppLink>
     </div>
-  </LandingSectionContainer>
+    <div class="hidden md:flex">
+      <img
+        class="self-center w-full pt-8 lg:pt-0"
+        src="@/assets/images/landing/squid-full.svg"
+        alt="kraken"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 import sailboat from '@iconify/icons-mdi/sail-boat';
 import AppLink from '@/components/common/AppLink.vue';
-import LandingSectionContainer from '@/components/app/landing/LandingSectionContainer.vue';
 import AppButtonStyle from '@/components/common/AppButtonStyle.vue';
 
 export default {
@@ -42,7 +39,6 @@ export default {
   components: {
     AppButtonStyle,
     AppLink,
-    LandingSectionContainer,
   },
 
   data() {
