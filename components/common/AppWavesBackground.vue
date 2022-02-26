bfe1b3bfbeff76459a9084340e3d900753a5b1b0
marhali <me@marhali.de>
2022-02-26 19:17:42 +0100

    feat(daisyUI): apply new theme structure

---
+++ components/common/AppWavesBackground.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="waves">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppWavesBackground',
+};
+</script>
+
+<style scoped>
+.waves {
+  background-image: url('./assets/images/landing/waves.svg');
+  background-position: center top;
+  background-repeat: no-repeat;
+  background-size: 100%;
+}
+
+@media only screen and (max-width: 1100px) {
+  .waves {
+    background-position: center center;
+  }
+}
+</style>
