165e777b69a608c3672e094709c440aa5d68d972
marhali <me@marhali.de>
2022-03-06 14:16:43 +0100

    feat(nav): add home link on mobile variant

--- components/app/header/PrimaryNavbar.vue
+++ components/app/header/PrimaryNavbar.vue
@@ -44,6 +44,12 @@
           <ul
             class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52 space-y-2"
           >
+            <li>
+              <nuxt-link to="/" exact active-class="active">
+                {{ $t('navbar.navbarLinkDefault') }}
+              </nuxt-link>
+            </li>
+
             <li v-for="page in pages" :key="page.to">
               <nuxt-link :to="page.to" active-class="active">
                 {{ page.title }}
