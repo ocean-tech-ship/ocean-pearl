f6986134b19356eaa9132dae6f0a36202de53c84
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2022-03-08 14:00:44 +0100

    feat(FooterContainer): rework footer

--- components/app/footer/FooterContainer.vue
+++ components/app/footer/FooterContainer.vue
@@ -1,111 +1,153 @@
 <template>
-  <footer class="w-full">
-    <div
-      class="
-        w-full
-        max-w-1440
-        mx-auto
-        py-10
-        grid
-        gap-10
-        md:flex md:items-center md:gap-0 md:justify-center
-        lg:gap-5
-      "
-    >
-      <div v-for="(item, i) in footerItems" :key="item.id">
-        <footer-block
-          :theme="item.theme"
-          :item="item"
-          :class="{ 'md:mr-20 lg:mr-0': i < footerItems.length - 1 }"
-        />
-      </div>
-    </div>
-  </footer>
+  <div class="bg-base-200">
+    <section-container>
+      <footer class="footer py-10 text-base-content">
+        <div v-for="item in footer1" :key="item.id">
+          <div class="grid grid-flow-col mb-2 uppercase font-bold">
+            <img
+              :src="item.img"
+              :alt="`${item.name} ${$t('general.logo')}`"
+              class="w-5 h-5 block mx-auto"
+            />
+            <span :class="`${item.class} ml-2`">{{ item.name }}</span>
+          </div>
+          <app-link
+            v-for="subItem in item.subItems"
+            :key="subItem.id"
+            :to="subItem.route"
+            class="link link-hover"
+            >{{ subItem.name }}</app-link
+          >
+        </div>
+      </footer>
+      <footer
+        class="footer py-4 border-t bg-base-200 text-base-content border-base-300"
+      >
+        <p class="text-2xs mb-0">
+          {{ footer2.name }}
+        </p>
+        <div class="md:place-self-center md:justify-self-end">
+          <div class="grid grid-flow-col gap-4">
+            <app-link
+              v-for="subItem in footer2.subItems"
+              :key="subItem.id"
+              :to="subItem.route"
+            >
+              <app-icon :data="subItem.icon" width="30" height="30" />
+            </app-link>
+          </div>
+        </div>
+      </footer>
+    </section-container>
+  </div>
 </template>
 
 <script>
-import Vue from 'vue';
-import twitter from '@iconify/icons-la/twitter-square';
+import twitter from '@iconify/icons-la/twitter';
 import discord from '@iconify/icons-la/discord';
-import github from '@iconify/icons-la/github-square';
-import chartLine from '@iconify/icons-la/chart-line';
-import scroll from '@iconify/icons-la/scroll';
-import FooterBlock from './FooterBlock.vue';
+import github from '@iconify/icons-la/github';
+import linkedin from '@iconify/icons-la/linkedin';
+import AppLink from '@/components/common/AppLink.vue';
+import AppIcon from '@/components/common/AppIcon.vue';
+import SectionContainer from '@/components/common/SectionContainer';
 
-export default Vue.extend({
-  components: { FooterBlock },
+export default {
+  components: {
+    SectionContainer,
+    AppLink,
+    AppIcon,
+  },
+  // TODO 1: Add translations
+  // TODO 2: Add SVGs for dark mode
   data() {
     return {
-      footerItems: [
+      footer1: [
         {
           id: 0,
           name: 'Ocean Pearl',
-          theme: 'text-primary',
           // eslint-disable-next-line global-require
           img: require('@/assets/images/logo/ocean-pearl.svg'),
           route: '/',
-          block: true,
-          hasSubItem: true,
+          class: 'text-primary',
           subItems: [
             {
               id: 0,
-              name: 'Twitter',
-              icon: twitter,
+              name: 'Analytics',
               route: 'https://twitter.com/oceanpearlio',
-              ext: true,
             },
             {
               id: 1,
-              name: 'Discord',
-              icon: discord,
-              route: 'https://discord.gg/eswPj8QZRH',
-              ext: true,
+              name: 'Server Status',
+              route: 'https://stats.uptimerobot.com/l0923I1O3y',
             },
             {
               id: 2,
-              name: 'GitHub',
-              icon: github,
+              name: 'Contact',
               route: 'https://github.com/ocean-tech-ship',
-              ext: true,
-            },
-            {
-              id: 3,
-              name: this.$t('footer.analytics'),
-              icon: chartLine,
-              route: 'https://plausible.io/oceanpearl.io',
-              ext: true,
             },
           ],
         },
         {
           id: 1,
           name: 'Ocean Protocol',
-          theme: 'text-third',
           // eslint-disable-next-line global-require
           img: require('@/assets/images/logo/ocean-protocol.svg'),
           route: 'https://oceanprotocol.com/',
-          ext: true,
-          block: true,
-          hasSubItem: true,
+          class: 'base-200',
           subItems: [
             {
-              id: 4,
-              name: 'Whitepaper',
-              icon: scroll,
-              route: 'https://oceanprotocol.com/tech-whitepaper.pdf',
-              ext: true,
+              id: 0,
+              name: 'Website',
+              route: 'https://oceanprotocol.com',
             },
             {
-              id: 5,
+              id: 1,
+              name: 'Ocean Port',
+              route: 'https://port.oceanprotocol.com/',
+            },
+            {
+              id: 2,
+              name: 'Github',
+              route: 'https://github.com/oceanprotocol',
+            },
+            {
+              id: 3,
               name: 'Discord',
-              icon: discord,
               route: 'https://discord.gg/Yuawx9EP6e',
-              ext: true,
             },
           ],
         },
       ],
+      footer2: {
+        name: '© 2022 Ocean Pearl - MIT License',
+        subItems: [
+          {
+            id: 0,
+            name: 'Twitter',
+            icon: twitter,
+            route: 'https://twitter.com/oceanpearlio',
+          },
+          {
+            id: 1,
+            name: 'Discord',
+            icon: discord,
+            route: 'https://discord.gg/eswPj8QZRH',
+          },
+          {
+            id: 2,
+            name: 'GitHub',
+            icon: github,
+            route: 'https://github.com/ocean-tech-ship',
+          },
+          {
+            id: 3,
+            name: 'Linkedin',
+            icon: linkedin,
+            route: 'https://linkedin.com/company/oceanpearl',
+          },
+        ],
+      },
     };
   },
-});
+};
 </script>
