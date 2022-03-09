7854f1817995e990aa5732802e8e1f3d31f9512c
marhali <me@marhali.de>
2022-03-09 22:09:31 +0100

    feat(daisyUI): create text input components

---
+++ components/common/AppTextInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <input
+    :type="type"
+    :placeholder="placeholder"
+    :value="value"
+    :disabled="disabled"
+    class="input w-full"
+  />
+</template>
+
+<script>
+export default {
+  name: 'AppTextInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+      default: null,
+    },
+    value: {
+      type: String,
+      default: null,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
