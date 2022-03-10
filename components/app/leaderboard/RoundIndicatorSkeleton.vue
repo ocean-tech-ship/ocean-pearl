ac216e2514ea6deb10fa65b5cf7382bf7c60043b
marhali <me@marhali.de>
2022-03-10 10:04:27 +0100

    feat(leaderboard): adjust theme configuration

--- components/app/leaderboard/RoundIndicatorSkeleton.vue
+++ components/app/leaderboard/RoundIndicatorSkeleton.vue
@@ -4,7 +4,7 @@
     class="rounded bg-base-200 px-4 py-2 flex flex-col items-center justify-center"
   >
     <div>{{ $t('leaderboard.round.title') }}</div>
-    <div class="w-9 h-9 rounded bg-primary opacity-70 animate-pulse" />
+    <div class="w-9 h-9 mx-9 rounded bg-primary opacity-70 animate-pulse" />
   </div>
 </template>
 
--- components/app/leaderboard/VotingCountdownSkeleton.vue
+++ components/app/leaderboard/VotingCountdownSkeleton.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div
-      class="flex items-center justify-around rounded border border-base-200"
+      class="flex items-center justify-around rounded border border-primary-content"
     >
       <div
         v-for="i in 4"
@@ -9,15 +9,19 @@
         class="p-2 md:p-4 flex flex-col items-center"
       >
         <div
-          class="rounded opacity-70 bg-base-200 animate-pulse w-8 h-8 mb-1"
+          class="rounded opacity-70 bg-primary-content animate-pulse w-8 h-8 mb-1"
         />
 
-        <div class="rounded opacity-70 bg-base-200 animate-pulse w-12 h-6" />
+        <div
+          class="rounded opacity-70 bg-primary-content animate-pulse w-12 h-6"
+        />
       </div>
     </div>
 
     <div class="flex justify-center h-10">
-      <div class="w-1/3 h-4 m-2 rounded opacity-70 bg-base-200 animate-pulse" />
+      <div
+        class="w-1/3 h-4 m-2 rounded opacity-70 bg-primary-content animate-pulse"
+      />
     </div>
   </div>
 </template>
