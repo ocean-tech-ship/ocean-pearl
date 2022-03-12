7e740bcd9c459a7fc96d4e6e1867280e31ffb4e6
marhali <me@marhali.de>
2022-03-12 18:26:10 +0100

    feat(manage): implement new landing ui

---
+++ components/app/manage/ActionCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <app-card class="relative bg-base-200 rounded shadow flex justify-center">
+    <!-- unlock overlay -->
+    <div
+      v-if="card.key !== 'create-project'"
+      class="absolute rounded bg-neutral bg-opacity-80 z-label w-full h-full flex flex-col items-center"
+    >
+      <div class="flex-grow flex items-center">
+        <div class="bg-base-200 p-4 rounded-full text-primary">
+          <app-icon class="md:hidden" :size="24" :data="icons.lock" />
+          <app-icon class="hidden md:block" :size="48" :data="icons.lock" />
+        </div>
+      </div>
+
+      <span class="hidden md:block pb-6">
+        {{ $t('manage.landing.unlock') }}
+      </span>
+    </div>
+
+    <!-- expand action (until md) -->
+    <div class="md:hidden absolute p-4 w-full flex justify-end text-primary">
+      <button
+        type="button"
+        class="btn btn-primary btn-circle btn-sm"
+        @click="expand = !expand"
+      >
+        <app-icon :size="24" :data="icons.info" />
+      </button>
+    </div>
+
+    <!-- content -->
+    <div class="flex flex-col items-center p-4 space-y-6">
+      <div class="flex items-center text-primary">
+        <app-icon class="pr-2" :size="32" :data="card.icon" />
+        <h4 class="text-base">
+          {{ $t('manage.landing.cards.' + card.key + '.title')[0] }}
+          <span class="text-base-content">
+            {{ $t('manage.landing.cards.' + card.key + '.title')[1] }}
+          </span>
+        </h4>
+      </div>
+
+      <img
+        class="hidden md:block w-2/3 h-32 justify-center"
+        :src="card.img"
+        alt=""
+      />
+
+      <p v-if="expand" class="small-text px-4 text-base-content text-center">
+        {{ $t('manage.landing.cards.' + card.key + '.subtitle') }}
+      </p>
+
+      <p class="hidden md:block small-text px-4 text-base-content text-center">
+        {{ $t('manage.landing.cards.' + card.key + '.subtitle') }}
+      </p>
+
+      <button v-if="expand" type="button" class="btn btn-outline btn-primary">
+        {{ $t('manage.landing.cards.' + card.key + '.action') }}
+      </button>
+
+      <button type="button" class="hidden md:block btn btn-outline btn-primary">
+        {{ $t('manage.landing.cards.' + card.key + '.action') }}
+      </button>
+    </div>
+  </app-card>
+</template>
+
+<script>
+import info from '@iconify/icons-la/info';
+import lock from '@iconify/icons-la/lock';
+import AppCard from '@/components/common/AppCard';
+import AppIcon from '@/components/common/AppIcon';
+
+export default {
+  name: 'ActionCard',
+  components: { AppIcon, AppCard },
+
+  props: {
+    card: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      expand: false,
+
+      icons: {
+        info,
+        lock,
+      },
+    };
+  },
+};
+</script>
---
+++ pages/manage/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-gradient-primary py-12">
+    <section-container class="text-primary-content">
+      <h3>{{ $t('manage.landing.title') }}</h3>
+      <p>{{ $t('manage.landing.subtitle') }}</p>
+
+      <div class="py-12 grid xl:grid-cols-3 gap-8">
+        <action-card v-for="card in cards" :key="card.key" :card="card" />
+      </div>
+    </section-container>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import plus from '@iconify/icons-la/plus';
+import clipboardList from '@iconify/icons-la/clipboard-list';
+import star from '@iconify/icons-la/star';
+import SectionContainer from '@/components/common/SectionContainer';
+import ActionCard from '~/components/app/manage/ActionCard';
+
+export default Vue.extend({
+  components: { ActionCard, SectionContainer },
+
+  data() {
+    return {
+      cards: [
+        {
+          key: 'create-project',
+          icon: plus,
+          img: require('assets/images/manage/create-project.svg'),
+        },
+        {
+          key: 'manage-project',
+          icon: clipboardList,
+          img: require('assets/images/manage/manage-project.svg'),
+        },
+        {
+          key: 'create-proposal',
+          icon: star,
+          img: require('assets/images/manage/create-proposal.svg'),
+        },
+      ],
+    };
+  },
+});
+</script>
