14bf00a1a62d4b86abd50a100922bfeae899dbfd
marhali <me@marhali.de>
2022-03-13 20:56:05 +0100

    feat(manage): implement stepper structure

---
+++ components/common/AppStepper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <slot :name="step" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppStepper',
+
+  props: {
+    step: {
+      type: Number,
+      default: 0,
+    },
+  },
+};
+</script>
--- pages/manage/projects/new.vue
+++ pages/manage/projects/new.vue
@@ -1,26 +1,53 @@
 <template>
   <manage-scaffold>
     <template #navigation>
+      <!-- stepper navigation -->
       <ul class="steps steps-vertical py-8">
         <li
-          v-for="step in $t('creator.project.steps')"
-          :key="step"
+          v-for="(stepName, index) in $t('creator.project.steps')"
+          :key="index"
+          :class="{ 'step-primary': index === step }"
           class="step"
         >
-          {{ step }}
+          <button
+            type="button"
+            class="btn btn-ghost font-normal mx-2"
+            @click="step = index"
+          >
+            {{ stepName }}
+          </button>
         </li>
       </ul>
     </template>
-    new project
+    <main class="px-8">
+      <!-- walk through project creation -->
+      <!-- every step needs to be registered here -->
+      <app-stepper :step="step">
+        <template #0>
+          <div>first</div>
+        </template>
+        <template #1>
+          <div>second</div>
+        </template>
+      </app-stepper>
+    </main>
   </manage-scaffold>
 </template>
 
 <script>
 import Vue from 'vue';
 import ManageScaffold from '@/components/app/manage/ManageScaffold';
+import AppStepper from '@/components/common/AppStepper';
 
 export default Vue.extend({
-  components: { ManageScaffold },
+  components: { AppStepper, ManageScaffold },
+
   layout: 'manage',
+
+  data() {
+    return {
+      step: 0,
+    };
+  },
 });
 </script>
