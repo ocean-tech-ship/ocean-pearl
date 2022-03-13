e4c405d0cb17ffe36cff579ff52f9c5b9d095d24
marhali <me@marhali.de>
2022-03-13 15:22:04 +0100

    feat(layouts): add layout for management pages

---
+++ layouts/manage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <nuxt />
+    <primary-footer />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import PrimaryFooter from '@/components/app/footer/PrimaryFooter';
+
+export default Vue.extend({
+  components: { PrimaryFooter },
+});
+</script>
