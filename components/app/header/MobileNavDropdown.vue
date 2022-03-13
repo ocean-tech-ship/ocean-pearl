db6768ea453e5b47f3ca652da4631360d8312bab
marhali <me@marhali.de>
2022-03-13 21:26:27 +0100

    feat(navbar): modularize navbar

---
+++ components/app/header/MobileNavDropdown.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="dropdown dropdown-end">
+    <label tabindex="0" class="btn btn-ghost btn-circle">
+      <app-icon class="w-10 h-10 text-primary" :data="icons.bars" />
+    </label>
+    <ul
+      tabindex="0"
+      class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52 space-y-2"
+    >
+      <li>
+        <nuxt-link
+          to="/"
+          exact
+          active-class="active"
+          @click.native="removeFocus()"
+        >
+          {{ $t('navbar.navbarLinkDefault') }}
+        </nuxt-link>
+      </li>
+
+      <li v-for="page in pages" :key="page.to">
+        <nuxt-link
+          :to="page.to"
+          active-class="active"
+          @click.native="removeFocus()"
+        >
+          {{ page.title }}
+        </nuxt-link>
+      </li>
+
+      <hr class="my-2 text-base-300" />
+
+      <li>
+        <management-options />
+      </li>
+
+      <management-links />
+
+      <theme-switcher class="self-center" />
+    </ul>
+  </div>
+</template>
+
+<script>
+import bars from '@iconify/icons-la/bars';
+import AppIcon from '@/components/common/AppIcon';
+import ManagementOptions from '@/components/app/header/ManagementOptions';
+import ManagementLinks from '@/components/app/header/ManagementLinks';
+import ThemeSwitcher from '@/components/app/header/ThemeSwitcher';
+
+export default {
+  name: 'MobileNavDropdown',
+
+  components: { ThemeSwitcher, ManagementLinks, ManagementOptions, AppIcon },
+
+  props: {
+    pages: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      icons: {
+        bars,
+      },
+    };
+  },
+
+  methods: {
+    removeFocus() {
+      setTimeout(() => document.activeElement.blur(), 150);
+    },
+  },
+};
+</script>
+
--- components/app/header/PrimaryNavbar.vue
+++ components/app/header/PrimaryNavbar.vue
@@ -5,17 +5,7 @@
     <section-container class="navbar px-0 py-4 xl:py-16">
       <!-- corporate (always) -->
       <div class="navbar-start">
-        <nuxt-link
-          class="pl-0 btn btn-ghost normal-case text-lg text-primary"
-          to="/"
-        >
-          <img
-            class="pr-2"
-            src="@/assets/images/pearl-logo.svg"
-            :alt="$t('general.logo')"
-          />
-          {{ $t('navbar.logoText') }}
-        </nuxt-link>
+        <logo-branding class="text-primary" />
       </div>
 
       <!-- main pages (>= xl) -->
@@ -37,75 +27,32 @@
 
       <!-- mobile navbar (until xl) -->
       <div class="xl:hidden navbar-end">
-        <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle">
-            <app-icon class="w-10 h-10 text-primary" :data="icons.bars" />
-          </label>
-          <ul
-            tabindex="0"
-            class="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-200 rounded-box w-52 space-y-2"
-          >
-            <li>
-              <nuxt-link
-                to="/"
-                exact
-                active-class="active"
-                @click.native="removeFocus()"
-              >
-                {{ $t('navbar.navbarLinkDefault') }}
-              </nuxt-link>
-            </li>
-
-            <li v-for="page in pages" :key="page.to">
-              <nuxt-link
-                :to="page.to"
-                active-class="active"
-                @click.native="removeFocus()"
-              >
-                {{ page.title }}
-              </nuxt-link>
-            </li>
-
-            <hr class="my-2 text-base-300" />
-
-            <li>
-              <management-options />
-            </li>
-
-            <management-links />
-
-            <theme-switcher class="self-center" />
-          </ul>
-        </div>
+        <mobile-nav-dropdown :pages="pages" />
       </div>
     </section-container>
   </nav>
 </template>
 
 <script>
-import bars from '@iconify/icons-la/bars';
 import SectionContainer from '@/components/common/SectionContainer';
 import ManagementOptions from '@/components/app/header/ManagementOptions';
-import ManagementLinks from '@/components/app/header/ManagementLinks';
-import AppIcon from '@/components/common/AppIcon';
 import ThemeSwitcher from '@/components/app/header/ThemeSwitcher';
+import LogoBranding from '@/components/app/header/LogoBranding';
+import MobileNavDropdown from '@/components/app/header/MobileNavDropdown';
 
 export default {
   name: 'PrimaryNavbar',
 
   components: {
+    MobileNavDropdown,
+    LogoBranding,
     SectionContainer,
     ManagementOptions,
-    ManagementLinks,
-    AppIcon,
     ThemeSwitcher,
   },
 
   data() {
     return {
-      icons: {
-        bars,
-      },
       pages: [
         {
           title: this.$t('navbar.navbarProjects'),
@@ -121,12 +68,6 @@ export default {
         },
       ],
     };
-  },
-
-  methods: {
-    removeFocus() {
-      setTimeout(() => document.activeElement.blur(), 150);
-    },
   },
 };
 </script>
