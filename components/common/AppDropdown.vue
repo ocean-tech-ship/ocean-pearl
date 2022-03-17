eb36430975a35b7d8efcd8a9046f6c7488b41d70
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2022-03-17 19:19:49 +0100

    feat(AppDropdown): init new dropdown component

---
+++ components/common/AppDropdown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="dropdown">
+    <slot v-if="$slots.button" />
+
+    <!-- provide menu via default slot -->
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppDropdown',
+  components: {},
+  props: {
+    labelType: {
+      type: String,
+      default: null,
+    },
+    label: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
