f4f2e9613d13a9613b30ed5a7058382735b776b8
marhali <me@marhali.de>
2022-03-17 18:07:16 +0100

    fix(i18n): use correct method call

--- pages/manage/projects/new.head.js
+++ pages/manage/projects/new.head.js
@@ -1,6 +1,6 @@
 export default function createHead(config, i18n) {
   return {
-    title: i18n('creator.project.title'),
+    title: i18n.t('creator.project.title'),
     link: [{ rel: 'canonical', href: `${config.rootURL}/manage/projects/new` }],
   };
 }
