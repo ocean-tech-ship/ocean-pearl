20720284cb172b891686711bbb11360290c3937d
marhali <me@marhali.de>
2022-05-06 18:40:43 +0200

    fix(ui): apply new theme structure

--- pages/redirect/index.vue
+++ pages/redirect/index.vue
@@ -1,7 +1,7 @@
 <template>
   <app-primary-gradient-background class="grid place-items-center h-screen">
     <div
-      class="bg-white shadow rounded p-6 w-11/12 md:w-4/5 xl:w-1/3 text-center space-y-3 md:space-y-5"
+      class="bg-base-200 shadow rounded p-6 w-11/12 md:w-4/5 xl:w-1/3 text-center space-y-3 md:space-y-5"
     >
       <NuxtLink to="/">
         <div class="flex items-center justify-center">
@@ -14,28 +14,27 @@
 
       <div class="flex justify-center">
         <app-icon
-          class="text-complementary"
+          class="text-warning"
           :size="64"
           :data="icons.exclamationCircle"
         />
       </div>
 
-      <p class="text-primary leading-tight">
+      <p class="leading-tight">
         {{ $t('redirect.consent') }}
       </p>
 
-      <p class="break-all text-smbase leading-tight">{{ $route.query.url }}</p>
+      <p class="break-all text-smbase leading-tight italic">
+        {{ $route.query.url }}
+      </p>
 
-      <div>
-        <a
-          :href="decodeURIComponent($route.query.url)"
-          rel="noopener noreferrer"
-        >
-          <div class="btn-gradient-primary-variant border-0">
-            {{ $t('general.continue') }}
-          </div>
-        </a>
-      </div>
+      <a
+        class="w-full btn btn-primary border-0"
+        :href="decodeURIComponent($route.query.url)"
+        rel="noopener noreferrer"
+      >
+        {{ $t('general.continue') }}
+      </a>
     </div>
   </app-primary-gradient-background>
 </template>
@@ -45,12 +44,10 @@ import exclamationCircle from '@iconify/icons-la/exclamation-circle';
 import Vue from 'vue';
 import AppPrimaryGradientBackground from '@/components/common/AppPrimaryGradientBackground';
 import AppIcon from '@/components/common/AppIcon';
-import AppButton from '@/components/common/AppButton';
 
 export default Vue.extend({
   name: 'RedirectConsent',
   components: {
-    AppButton,
     AppIcon,
     AppPrimaryGradientBackground,
   },
