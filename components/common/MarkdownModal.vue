a18b30c4536808191e77189b43ddee8bf5fc8b50
marhali <me@marhali.de>
2022-05-22 21:36:09 +0200

    feat(components): add markdown modal component

---
+++ components/common/MarkdownModal.vue
@@ -0,0 +1,41 @@
+<template>
+  <app-modal :open="open" close-button large>
+    <template #body>
+      <div
+        v-dompurify-html="renderedMarkdown"
+        class="prose break-words max-w-none xl:p-4"
+      />
+    </template>
+  </app-modal>
+</template>
+
+<script>
+import AppModal from '@/components/common/AppModal';
+import Markdown from '@/mixins/Markdown';
+
+export default {
+  name: 'ProposalModal',
+
+  components: { AppModal },
+
+  mixins: [Markdown],
+
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    markdown: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    renderedMarkdown() {
+      return this.renderMarkdown(this.$props.markdown);
+    },
+  },
+};
+</script>
