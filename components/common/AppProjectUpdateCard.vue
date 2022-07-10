8fc42c0b52a93c26b49be993dea2e6040b06f0ad
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2022-07-10 13:24:20 +0200

    feat(AppProjectUpdateCard): create new card component for landing and updates pages

---
+++ components/common/AppProjectUpdateCard.vue
@@ -0,0 +1,68 @@
+<template>
+  <app-link-card
+    :prefetch="false"
+    :to="`/project-updates/${beautifyProjectId(projectUpdate)}`"
+    :card-class="`card bg-base-200 shadow rounded flex p-8 h-[264px] ${!singleColVariant} && lg:h-275px`"
+  >
+    <div :class="{ 'md:min-h-[62px]': !singleColVariant }">
+      <p class="text-primary leading-snug line-clamp-2">
+        {{ projectUpdate.title }}
+      </p>
+    </div>
+
+    <div class="mt-5 h-128px">
+      <p class="small-text line-clamp-2">
+        {{ projectUpdate.oneLiner }}
+      </p>
+    </div>
+
+    <div class="mt-4 flex items-center">
+      <app-logo class="mr-4" :project="projectUpdate.project" :size="34" />
+      <div class="flex items-start flex-col">
+        <p class="small-text text-primary line-clamp-1">
+          {{ projectUpdate.project.title }}
+        </p>
+        <p class="small-text">
+          {{ formatDistance(projectUpdate.createdAt) }}
+        </p>
+      </div>
+    </div>
+  </app-link-card>
+</template>
+
+<script>
+import ProjectBeautifyId from '@/mixins/ProjectBeautifyId';
+import AppLogo from '@/components/common/AppLogo.vue';
+import AppLinkCard from '@/components/common/AppLinkCard.vue';
+
+export default {
+  name: 'AppProjectUpdateCard',
+
+  components: {
+    AppLogo,
+    AppLinkCard,
+  },
+
+  mixins: [ProjectBeautifyId],
+
+  props: {
+    singleColVariant: {
+      type: Boolean,
+      default: false,
+    },
+    projectUpdate: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  methods: {
+    formatDistance(timestamp) {
+      return this.$dateFns.formatDistanceToNowStrict(new Date(timestamp), {
+        addSuffix: true,
+        locale: this.$i18n.locale,
+      });
+    },
+  },
+};
+</script>
