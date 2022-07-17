fd9ca9ba7870810b45dedacc16e742083e4782e1
Brian Boehm <62890942+Zuckerwattederivat@users.noreply.github.com>
2022-07-17 16:58:44 +0200

    feat(project-updates): create project-updates page

---
+++ components/app/project-updates/ProjectUpdatesHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>
+      {{ $t('projectUpdates.header.title') }}
+      <span class="text-primary">{{
+        $t('projectUpdates.header.titleHighlight')
+      }}</span>
+    </h2>
+    <p>{{ $t('projectUpdates.header.text') }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectUpdatesHeader',
+};
+</script>
