f40ccce0a7c925bd00dce80c60af90827460e1e4
marhali <me@marhali.de>
2022-07-19 22:09:56 +0200

    feat(creation): enhance final

--- components/app/manage/creator/project/steps/FinalStep.vue
+++ components/app/manage/creator/project/steps/FinalStep.vue
@@ -1,5 +1,18 @@
 <template>
-  <FormControl label="Last but not least"> finals </FormControl>
+  <FormControl label="Last but not least">
+    <span>
+      Ocean Pearl is all about community. We highly appreciate that you want to
+      list your project and share updates with the community. <br />
+      Together we can improve the entire ecosystem.
+    </span>
+
+    <span class="pt-4">
+      You are only one step away from submitting your project to Ocean Pearl.
+      <br />
+      After submission we will take a manual review of your project and list it
+      if it does not violate our guidelines.
+    </span>
+  </FormControl>
 </template>
 
 <script>
