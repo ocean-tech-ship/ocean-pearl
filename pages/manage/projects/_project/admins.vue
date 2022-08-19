fb84e0a52fbbb7a2d282a01660942bc4fd853c4d
marhali <me@marhali.de>
2022-08-19 21:59:09 +0200

    feat(manage): finalize new admins page

--- pages/manage/projects/_project/admins.vue
+++ pages/manage/projects/_project/admins.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
     <h4>
-      Project
-      <span class="text-primary"> Administrators </span>
+      {{ $t('manage.admins.title')[0] }}
+      <span class="text-primary">
+        {{ $t('manage.admins.title')[1] }}
+      </span>
     </h4>
+
+    <span>
+      {{ $t('manage.admins.subtitle') }}
+    </span>
+
+    <project-admins
+      :project="project"
+      @change="$emit('change', { accessAddresses: $event })"
+    />
   </div>
 </template>
 
 <script>
 import Vue from 'vue';
+import ProjectAdmins from '@/components/app/manage/project/ProjectAdmins';
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { ProjectAdmins },
+
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    },
+  },
+});
 </script>
