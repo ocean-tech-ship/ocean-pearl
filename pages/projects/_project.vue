cdcb2494ec7aec390fb719ba24a1167fb7f465fc
marhali <me@marhali.de>
2022-10-02 14:36:14 +0200

    refactor(project): remove unused import

--- pages/projects/_project.vue
+++ pages/projects/_project.vue
@@ -88,7 +88,6 @@ export default Vue.extend({
 
   components: {
     ProjectSinglePosts,
-    // ProjectSingleRoiStrategy,
     SectionContainer,
     ProjectSingleDescription,
     ProjectSingleHeader,
